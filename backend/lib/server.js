c442ad5b721c4118c3bf73fd4f1ac8d45bb37fa6
Martijn Vegter <martijn@martijnvegter.com>
2020-03-21 17:27:36 +0100

    Added basic backend

---
+++ backend/lib/server.js
@@ -0,0 +1,30 @@
+/**
+ * This file is part of the ALICE Electronic Logbook v2, also known as Jiskefet.
+ * Copyright (C) 2020  Stichting Hogeschool van Amsterdam
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published
+ * by the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+const { HttpServer } = require('@aliceo2/web-ui');
+
+const http = new HttpServer({
+    port: 4000
+});
+
+http.get('/', (_request, response, _next) => {
+    response.status(200).json({
+        name: 'Jiskefet Backend',
+        version: '0.0.0',
+    });
+}, { public: true });
