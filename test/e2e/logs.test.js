d4048d915fe398aa80a29a3576ee4b8aa5fbf477
Martijn Vegter <martijn@martijnvegter.com>
2020-04-29 12:13:12 +0200

    test: added logs test

---
+++ test/e2e/logs.test.js
@@ -0,0 +1,70 @@
+/**
+ * @license
+ * Copyright CERN and copyright holders of ALICE O2. This software is
+ * distributed under the terms of the GNU General Public License v3 (GPL
+ * Version 3), copied verbatim in the file "COPYING".
+ *
+ * See http://alice-o2.web.cern.ch/license for full licensing information.
+ *
+ * In applying this license CERN does not waive the privileges and immunities
+ * granted to it by virtue of its status as an Intergovernmental Organization
+ * or submit itself to any jurisdiction.
+ */
+
+const path = require('path');
+const chai = require('chai');
+const request = require('supertest');
+const chaiResponseValidator = require('chai-openapi-response-validator');
+
+const { expect } = chai;
+
+chai.use(chaiResponseValidator(path.resolve(__dirname, '..', '..', 'spec', 'openapi.yaml')));
+
+module.exports = () => {
+    describe('POST /api/logs', () => {
+        const { server } = require('../../lib/application');
+
+        it('should return 400 if no title is provided', (done) => {
+            request(server)
+                .post('/api/logs')
+                .expect(400)
+                .end((err, res) => {
+                    if (err) {
+                        done(err);
+                        return;
+                    }
+
+                    // Response must satisfy the OpenAPI specification
+                    expect(res).to.satisfyApiSpec;
+
+                    const titleError =  res.body.errors.find((err) => err.source.pointer === '/data/attributes/title');
+                    expect(titleError.detail).to.equal('Title must contain at least three characters.');
+
+                    done();
+                });
+        });
+
+        it('should return 201 if a proper body was sent', (done) => {
+            request(server)
+                .post('/api/logs')
+                .send({
+                    title: 'Yet another run',
+                })
+                .expect(201)
+                .end((err, res) => {
+                    if (err) {
+                        done(err);
+                        return;
+                    }
+
+                    // Response must satisfy the OpenAPI specification
+                    expect(res).to.satisfyApiSpec;
+
+                    expect(res.body.data).to.have.lengthOf(1);
+                    expect(res.body.data[0].title).to.equal('Yet another run');
+
+                    done();
+                });
+        });
+    });
+};
