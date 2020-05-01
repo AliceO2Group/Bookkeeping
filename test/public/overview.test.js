c1035d5205e2d823d1b6df0ad17bfd6c583d1786
Martijn Vegter <martijn@martijnvegter.com>
2020-05-01 11:17:09 +0200

    test: removed assert package usage in Overview test

--- test/public/overview.test.js
+++ test/public/overview.test.js
@@ -11,10 +11,12 @@
  * or submit itself to any jurisdiction.
  */
 
-const assert = require('assert');
+const chai = require('chai');
 const puppeteer = require('puppeteer');
 const pti = require('puppeteer-to-istanbul');
 const { server } = require('../../lib/application');
+
+const { expect } = chai;
 
 module.exports = function () {
     // Configure this suite to have a default timeout of 5s
@@ -55,33 +57,33 @@ module.exports = function () {
         await page.waitFor(100);
 
         // We expect the page to return the correct status code, making sure the server is running properly
-        assert.equal(response.status(), 200);
+        expect(response.status()).to.equal(200);
 
         // We expect the page to return the correct title, making sure there isn't another server running on this port
         const title = await page.title();
-        assert.equal(title, 'AliceO2 Bookkeeping 2020');
+        expect(title).to.equal('AliceO2 Bookkeeping 2020');
     });
 
     it('can filter logs dynamically', async () => {
         const checkbox = await page.$('.form-check input');
         const label = await page.$('.form-check label div');
 
+        // We expect to have captured the first checkbox in the list
         const id = await page.evaluate((element) => element.id, checkbox);
         const amount = await page.evaluate((element) => element.innerText, label);
-        // We expect to have captured the first checkbox in the list
-        assert.equal(id, 'filtersCheckbox1');
+        expect(id).to.equal('filtersCheckbox1');
 
         await page.click(`#${id}`);
         await page.waitFor(100);
 
         // We expect the amount of logs in this filter to match the advertised amount in the filters component
         const tableRows = await page.$$('table tr');
-        assert.equal(true, tableRows.length - 1 === parseInt(amount.substring(1, amount.length - 1)));
+        expect(parseInt(amount.substring(1, amount.length - 1))).to.equal(tableRows.length - 1);
 
         // Deselect the filter and wait for the changes to process
         await page.click(`#${id}`);
         await page.waitFor(100);
-        assert.equal(true, tableRows.length - 1 === parseInt(amount.substring(1, amount.length - 1)));
+        expect(parseInt(amount.substring(1, amount.length - 1))).to.equal(tableRows.length - 1);
     });
 
     it('can navigate to a log detail page', async () => {
@@ -93,10 +95,10 @@ module.exports = function () {
         await page.click(firstRow);
         await page.waitFor(100);
         const redirectedUrl = await page.url();
-        assert.equal(redirectedUrl, `${url}/?page=entry&id=${id}`);
+        expect(redirectedUrl).to.equal(`${url}/?page=entry&id=${id}`);
 
         // We expect there to be at least one post in this log entry
         const postExists = !!(await page.$('#post1'));
-        assert.equal(true, postExists);
+        expect(postExists).to.be.true;
     });
 };
