758699d1ad3be42c0dc07aadc335b0a3e2995c81
Martijn Vegter <martijn@martijnvegter.com>
2020-06-03 11:19:28 +0200

    refactor: implemented a builder for the where option of QueryBuilder

---
+++ test/database/utilities/QueryBuilder.test.js
@@ -0,0 +1,40 @@
+/**
+ * @license
+ * Copyright CERN and copyright holders of ALICE O2. This software is
+ * distributed under the terms of the GNU General Public License v3 (GPL
+ * Version 3), copied verbatim in the file "COPYING".
+ *
+ * See http://alice-o2.web.cern.ch/license for full licensing information.
+ *
+ * In applying this license CERN does not waive the privileges and immunities
+ * granted to it by virtue of its status as an Intergovernmental Organization
+ * or submit itself to any jurisdiction.
+ */
+
+const {
+    repositories: {
+        LogRepository,
+    },
+    utilities: {
+        QueryBuilder,
+    },
+} = require('../../../lib/database');
+const chai = require('chai');
+
+const { expect } = chai;
+
+module.exports = () => {
+    describe('WhereQueryBuilder', () => {
+        describe('is', () => {
+            it('should return a single entity with the provided id', async () => {
+                const queryBuilder = new QueryBuilder();
+                queryBuilder.where('id').is('1');
+
+                const result = await LogRepository.findAll(queryBuilder);
+                expect(result).to.not.be.null;
+                expect(result).to.have.lengthOf(1);
+                expect(result[0].id).to.equal(1);
+            });
+        });
+    });
+};
