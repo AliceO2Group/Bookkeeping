0f77e4ed9a30e2427f47a6177a9ca85e03f9632b
Martijn Vegter <martijn@martijnvegter.com>
2020-06-03 11:32:04 +0200

    feat: added oneOf and allOf options for WhereQueryBuilder

--- test/database/utilities/QueryBuilder.test.js
+++ test/database/utilities/QueryBuilder.test.js
@@ -36,5 +36,30 @@ module.exports = () => {
                 expect(result[0].id).to.equal(1);
             });
         });
+
+        describe('allOf', () => {
+            it('should return no entities', async () => {
+                const queryBuilder = new QueryBuilder();
+                queryBuilder.where('id').allOf(1, 2);
+
+                const result = await LogRepository.findAll(queryBuilder);
+                expect(result).to.not.be.null;
+                expect(result).to.have.lengthOf(0);
+            });
+        });
+
+        describe('oneOf', () => {
+            it('should return a single entity with the provided id', async () => {
+                const queryBuilder = new QueryBuilder();
+                queryBuilder.where('id').oneOf('1', 2);
+                queryBuilder.orderBy('id', 'asc');
+
+                const result = await LogRepository.findAll(queryBuilder);
+                expect(result).to.not.be.null;
+                expect(result).to.have.lengthOf(2);
+                expect(result[0].id).to.equal(1);
+                expect(result[1].id).to.equal(2);
+            });
+        });
     });
 };
