b3585246de0325a73d495b77e9b882e152f96c62
MauritsioRK <maurits_rk@hotmail.com>
2020-07-17 11:56:08 +0200

    feat: Add functionality to filter by author name

--- test/e2e/logs.test.js
+++ test/e2e/logs.test.js
@@ -84,6 +84,46 @@ module.exports = () => {
                 });
         });
 
+        it('should support filtering by author', (done) => {
+            request(server)
+                .get('/api/logs?filter[author]=john')
+                .expect(200)
+                .end((err, res) => {
+                    if (err) {
+                        done(err);
+                        return;
+                    }
+
+                    // Response must satisfy the OpenAPI specification
+                    expect(res).to.satisfyApiSpec;
+
+                    expect(res.body.data).to.be.an('array');
+                    expect(res.body.data.length).to.be.greaterThan(1);
+
+                    done();
+                });
+        });
+
+        it('should return 400 if the author filter is left empty', (done) => {
+            request(server)
+                .get('/api/logs?filter[author]= ')
+                .expect(400)
+                .end((err, res) => {
+                    if (err) {
+                        done(err);
+                        return;
+                    }
+
+                    // Response must satisfy the OpenAPI specification
+                    expect(res).to.satisfyApiSpec;
+
+                    const { errors } = res.body;
+                    expect(errors[0].detail).to.equal('"query.filter.author" is not allowed to be empty');
+
+                    done();
+                });
+        });
+
         it('should support filtering by creation time', (done) => {
             request(server)
                 .get('/api/logs?filter[created][from]=946684800000')
