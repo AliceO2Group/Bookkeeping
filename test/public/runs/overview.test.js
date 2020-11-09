dba0152130200678a53208bcf07dac4b9e8099bc
Roman Meshkov <36418657+Romjkez@users.noreply.github.com>
2020-11-09 18:03:46 +0300

    feat(Pagination): add option with input to amountSelector (#222)
    * feat(Pagination): add option with input to amountSelector
    
    * feat(amountSelector): add `on fly` validation to perPage input
    
    * lint(amountSelector): fix linting
    
    * test(amountSelector): cover case when user types invalid custom per page value
    
    * test(eslint): remove browser env

--- test/public/runs/overview.test.js
+++ test/public/runs/overview.test.js
@@ -142,6 +142,17 @@ module.exports = () => {
 
         const tableRows = await page.$$('table tr');
         expect(tableRows.length - 1).to.equal(5);
+
+        // Expect the custom per page input to have red border and text color if wrong value typed
+        const customPerPageInput = await page.$(`${amountSelectorId} input[type=number]`);
+        await customPerPageInput.evaluate((input) => input.focus());
+        await page.$eval(`${amountSelectorId} input[type=number]`, (el) => {
+            el.value = '111';
+            // eslint-disable-next-line no-undef
+            el.dispatchEvent(new Event('input'));
+        });
+        await page.waitForTimeout(100);
+        expect(Boolean(await page.$(`${amountSelectorId} .danger`))).to.be.true;
     });
 
     it('can switch between pages of runs', async () => {
