c4909250ad48e5c74da57f1b6a1fb44841c28ff8
Tim van Ekert <tim.van.ekert@hva.nl>
2021-02-08 16:53:16 +0100

    make new tests actually run

--- test/public/home/index.js
+++ test/public/home/index.js
@@ -11,18 +11,8 @@
  * or submit itself to any jurisdiction.
  */
 
-const LogsSuite = require('./logs');
-const RunsSuite = require('./runs');
-const SubsystemsSuite = require('./subsystems');
-const TagsSuite = require('./tags');
-const FlpsSuite = require('./flps');
-const HomeSuite = require('./home');
+const OverviewSuite = require('./overview.test');
 
 module.exports = () => {
-    describe('Logs', LogsSuite);
-    describe('Runs', RunsSuite);
-    describe('Subsystems', SubsystemsSuite);
-    describe('Tags', TagsSuite);
-    describe('Flps', FlpsSuite);
-    describe('Home', HomeSuite);
+    describe('Overview Page', OverviewSuite);
 };
