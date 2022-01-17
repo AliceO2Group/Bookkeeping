6c5e932fe0b0bceb5a1b07a36e8962d77273e4c9
whdegraaf <92573127+whdegraaf@users.noreply.github.com>
2022-01-17 11:04:51 +0100

    [O2B-460] Adding details button to logs/runs tables. (#476)
    * Added 'Actions' cell buttons on logs, runs and home page tables rather than the entire row being clickable.
    * This will allow the users to copy data straight from table
    * Modified tests to work with clickable button in table rows.
    
    Co-authored-by: George R <george.raduta@cern.ch>

--- test/public/logs/overview.test.js
+++ test/public/logs/overview.test.js
@@ -499,12 +499,12 @@ module.exports = () => {
         // Go back to the home page
         await goToPage(page, 'log-overview');
 
-        const firstRow = await page.$('tr.clickable');
-        const firstRowId = await firstRow.evaluate((row) => row.id);
-        parsedFirstRowId = parseInt(firstRowId.slice('row'.length, firstRowId.length), 10);
+        const firstButton = await page.$('button.clickable');
+        const firstRowId = await firstButton.evaluate((btn) => btn.id);
+        parsedFirstRowId = parseInt(firstRowId.slice('btn'.length, firstRowId.length), 10);
 
         // We expect the entry page to have the same id as the id from the log overview
-        await firstRow.evaluate((row) => row.click());
+        await firstButton.evaluate((button) => button.click());
         await page.waitForTimeout(500);
 
         const redirectedUrl = await page.url();
@@ -528,10 +528,10 @@ module.exports = () => {
         await page.waitForTimeout(500);
 
         // Navigate to a log detail page
-        table = await page.$$('tr');
-        firstRowId = await getFirstRow(table, page);
-        const row = await page.$(`tr#${firstRowId}`);
-        await row.evaluate((row) => row.click());
+        const firstRow = await page.$('button.clickable');
+        const firstRowId = await firstRow.evaluate((btn) => btn.id);
+        parsedFirstRowId = parseInt(firstRowId.slice('btn'.length, firstRowId.length), 10);
+        await firstRow.evaluate((button) => button.click());
         await page.waitForTimeout(500);
 
         // Go back to the home page again
