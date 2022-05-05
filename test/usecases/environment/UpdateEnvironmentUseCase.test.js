df8636ccf42ed6aecbebf9d1c8e9db130f5ed966
Jik Huijberts <jikhuijberts@gmail.com>
2022-05-05 09:25:07 +0200

    Removed the run value from updates

--- test/usecases/environment/UpdateEnvironmentUseCase.test.js
+++ test/usecases/environment/UpdateEnvironmentUseCase.test.js
@@ -28,7 +28,6 @@ module.exports = () => {
                 envId: 'EIDO13i3D',
             },
             body: {
-                run: 1,
                 toredownAt: new Date(),
                 status: 'STOPPED',
                 statusMessage: 'Completely new message',
@@ -45,7 +44,7 @@ module.exports = () => {
     it('Should be able to update the environment with correct values', async () => {
         const { result } = await new UpdateEnvironmentUseCase()
             .execute(updateEnvironmentDto);
-        expect(result.runs[0].id).to.equal(1);
+        expect(result.status).to.equal('STOPPED');
     });
 
     it('Should give an error when the id of the environment can not be found', async () => {
@@ -54,14 +53,6 @@ module.exports = () => {
             .execute(updateEnvironmentDto);
         expect(error.status).to.equal('400');
         expect(error.title).to.equal(`Environment with this envId (${wrongId}) could not be found`);
-    });
-
-    it('Should give an error when the id of the run can not be found', async () => {
-        updateEnvironmentDto.body.run = wrongId;
-        const { error } = await new UpdateEnvironmentUseCase()
-            .execute(updateEnvironmentDto);
-        expect(error.status).to.equal('400');
-        expect(error.title).to.equal(`Run with this id (${wrongId}) could not be found`);
     });
 
     it('Should give an error when the id can not be found', async () => {
