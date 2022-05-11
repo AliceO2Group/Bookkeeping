b8ac728e94e5ca45d2d249a171e6a72088448600
George Raduta <george.raduta@cern.ch>
2022-05-11 14:45:55 +0200

    Add usecase test suite for startrun

---
+++ test/usecases/run/StartRunUseCase.test.js
@@ -0,0 +1,59 @@
+/**
+ * @license
+ * Copyright CERN and copyright holders of ALICE O2. This software is
+ * distributed under the terms of the GNU General Public License v3 (GPL
+ * Version 3), copied verbatim in the file "COPYING".
+ *
+ * See http://alice-o2.web.cern.ch/license for full licensing information.
+ *
+ * In applying this license CERN does not waive the privileges and immunities
+ * granted to it by virtue of its status as an Intergovernmental Organization
+ * or submit itself to any jurisdiction.
+ */
+
+const { run: { StartRunUseCase } } = require('../../../lib/usecases');
+const { dtos: { StartRunDto } } = require('../../../lib/domain');
+const chai = require('chai');
+
+const { expect } = chai;
+
+module.exports = () => {
+    let startRunDto;
+
+    beforeEach(async () => {
+        startRunDto = await StartRunDto.validateAsync({
+            body: {
+                runNumber: 107,
+                timeO2Start: '2022-03-21 13:00:00',
+                timeTrgStart: '2022-03-21 13:00:00',
+                environmentId: '1234567890',
+                runType: 'technical',
+                runQuality: 'good',
+                nDetectors: 3,
+                nFlps: 10,
+                nEpns: 10,
+                dd_flp: true,
+                dcs: true,
+                epn: true,
+                epnTopology: 'normal',
+                detectors: 'CPV',
+            },
+        });
+    });
+    it('should successfully store and return the saved entity', async () => {
+        const { result } = await new StartRunUseCase()
+            .execute(startRunDto);
+        expect(result).to.be.an('object');
+        expect(result.id).to.equal(107);
+    });
+
+    it('should successfully store and return the saved entity with default values if not provided', async () => {
+        delete startRunDto.body.detectors;
+        startRunDto.body.runNumber = 108;
+        const { result } = await new StartRunUseCase()
+            .execute(startRunDto);
+        expect(result).to.be.an('object');
+        expect(result.id).to.equal(108);
+        expect(result.detectors).to.equal(null);
+    });
+};
