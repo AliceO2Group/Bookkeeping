77714e30929243689406e0aabb56de5d85984e23
Jik Huijberts <jikhuijberts@gmail.com>
2022-05-16 17:45:45 +0200

    All endpoints implemented and tested

---
+++ test/usecases/lhcFill/CreateLhcFillsUseCase.test.js
@@ -0,0 +1,58 @@
+/**
+ * @license
+ * Copyright CERN and copyright holders of ALICE O2. This software is
+ * distributed under the terms of the GNU General Public License v3 (GPL
+ * Version 3), copied verbatim in the file "COPYING".
+ *
+ * See http://alice-o2.web.cern.ch/license for full licensing information.
+ *
+ * In applying this license CERN does not waive the privileges and immunities
+ * granted to it by virtue of its status as an Intergovernmental Organization
+ * or submit itself to any jurisdiction.
+ */
+const { repositories: { LhcFillRepository } } = require('../../../lib/database');
+const { lhcFill: { CreateLhcFillUseCase } } = require('../../../lib/usecases');
+const { dtos: { CreateLhcFillDto } } = require('../../../lib/domain');
+const chai = require('chai');
+
+const { expect } = chai;
+
+module.exports = () => {
+    let createLhcFillDto;
+
+    beforeEach(async () => {
+        createLhcFillDto = await CreateLhcFillDto.validateAsync({
+            body: {
+                stableBeamsStart: new Date('2022-03-22 15:00:00'),
+                stableBeamsEnd: new Date('2022-03-22 15:00:00'),
+                stableBeamsDuration: 600,
+                beamType: 'Pb-Pb',
+                fillingSchemeName: 'schemename',
+            },
+        });
+        createLhcFillDto.session = {
+            personid: 1,
+            id: 1,
+            name: 'John Doe',
+        };
+    });
+    it('should insert a new LhcFill', async () => {
+        const nEnvsBefore = await LhcFillRepository.count();
+
+        await new CreateLhcFillUseCase()
+            .execute(createLhcFillDto);
+
+        const nEnvsAfter = await LhcFillRepository.count();
+        expect(nEnvsAfter).to.be.greaterThan(nEnvsBefore);
+    });
+    it('should insert a new lhcFill with the right values', async () => {
+        const { result } = await new CreateLhcFillUseCase()
+            .execute(createLhcFillDto);
+
+        expect(result.stableBeamsStart).to.equal(new Date('2022-03-22 15:00:00 utc').getTime());
+        expect(result.stableBeamsEnd).to.equal(new Date('2022-03-22 15:00:00 utc').getTime());
+        expect(result.stableBeamsDuration).to.equal(600);
+        expect(result.beamType).to.equal('Pb-Pb');
+        expect(result.fillingSchemeName).to.equal('schemename');
+    });
+};
