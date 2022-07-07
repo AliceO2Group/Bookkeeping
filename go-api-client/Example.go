85059a643d00fa06f60c7745b41999226657a0c5
Jik Huijberts <43553504+JikHuijberts@users.noreply.github.com>
2022-07-07 16:14:06 +0200

    [O2B-631] && [O2B-632] Create Run & Update Run OpenAPI (#640)
    For go:
    Extra fields are added for updating the run:
    
    timeO2Start
    timeTrgStart
    trgGlobalRunEnabled
    trgEnabled
    pdpConfigOption
    pdpTopologyDescriptionLibraryFile
    tfbDdMode
    Two fields are removed for the create run function:
    
    timeO2Start
    timeTrg

--- go-api-client/Example.go
+++ go-api-client/Example.go
@@ -1,8 +1,6 @@
 package main
 
 import (
-	"time"
-
 	apiClient "github.com/AliceO2Group/Bookkeeping/go-api-client/src"
 	sw "github.com/AliceO2Group/Bookkeeping/go-api-client/src/go-client-generated"
 )
@@ -13,18 +11,19 @@ func main() {
 	apiToken := "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6MCwidXNlcm5hbWUiOiJhbm9ueW1vdXMiLCJuYW1lIjoiQW5vbnltb3VzIiwiYWNjZXNzIjowLCJpYXQiOjE2MzM2ODQ3MDcsImV4cCI6MTY2NTI0MjMwNywiaXNzIjoibzItdWkifQ.bWbXX5tSgElHCx9bEeRRjGbd2ISckPpCYSwJnOh8mV0"
 
 	// Create an Unix Timestamp to local time.
-	testTime := time.Unix(time.Now().Unix(), 0)
+
+	testTime := int64(1656936788677)
 
 	// Initialize api with manual JWT token + baseurl
 	// TODO: generate correct JWT token instead of manual insertion
 	apiClient.InitializeApi(baseUrl, apiToken)
 
 	// Create a run
-	apiClient.CreateRun("go-api-Timestamp", 5, 5, 1, 80, sw.COSMICS_RunType, testTime, testTime, false, false, true, "normal",
+	apiClient.CreateRun("go-api-Timestamp", 5, 5, 1, 80, sw.COSMICS_RunType, false, false, true, "normal",
 		(sw.CPV_Detectors + "," + sw.ZDC_Detectors + "," + sw.EMC_Detectors))
 
 	// Update a run
-	apiClient.UpdateRun(80, sw.BAD_RunQuality, testTime, testTime)
+	apiClient.UpdateRun(80, sw.BAD_RunQuality, -1, testTime, -1, testTime, false, false, "Repository Hash", "production/production.desc", "processing")
 
 	// Create an flp
 	apiClient.CreateFlp("flpTestName", "flpTestHost", 80)
