d306f9b21b1fadc004c19630ff7c9e77f59deda0
martinboulais <31805063+martinboulais@users.noreply.github.com>
2023-02-10 08:00:14 +0100

    Fix test

--- test/lib/usecases/environment/UpdateEnvironmentUseCase.test.js
+++ test/lib/usecases/environment/UpdateEnvironmentUseCase.test.js
@@ -44,7 +44,7 @@ module.exports = () => {
     it('Should be able to update the environment with correct values', async () => {
         const { result } = await new UpdateEnvironmentUseCase()
             .execute(updateEnvironmentDto);
-        expect(result.status).to.equal('STOPPED');
+        expect(result.status).to.equal('DESTROYED');
     });
 
     it('Should give an error when the id of the environment can not be found', async () => {
