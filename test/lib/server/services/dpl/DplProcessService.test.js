ea18517a6600ceacdd6f505350ce892e1d5dac48
Martin Boulais <31805063+martinboulais@users.noreply.github.com>
2023-05-10 11:03:00 +0200

    [O2B-867] Fetch all DPL processes executed for a given run and detector

--- test/lib/server/services/dpl/DplProcessService.test.js
+++ test/lib/server/services/dpl/DplProcessService.test.js
@@ -24,10 +24,30 @@ module.exports = () => {
         expect(detectors.every(({ processesExecutions }) => processesExecutions.length === 0)).to.be.true;
     });
 
-    it('should throw an error if the given run do not exist', async () => {
+    it('should throw an error if trying to fetch detectors for a run that does not exist', async () => {
         await assert.rejects(
             () => dplProcessService.getAllDetectorsWithExecutedProcessesByRun({ runNumber: 999 }),
             new NotFoundError('Run with this run number (999) could not be found'),
+        );
+    });
+
+    it('should successfully return the list of processes that have an executed process for a given run and a given detector', async () => {
+        const processes = await dplProcessService.getAllExecutedProcessesByRunAndDetector({ runNumber: 106 }, 1);
+        expect(processes.map(({ id }) => id)).to.eql([1, 2, 3]);
+        expect(processes.every(({ processesExecutions }) => processesExecutions.length === 0)).to.be.true;
+    });
+
+    it('should throw an error if trying to fetch processes for a run that does not exist', async () => {
+        await assert.rejects(
+            () => dplProcessService.getAllExecutedProcessesByRunAndDetector({ runNumber: 999 }, 1),
+            new NotFoundError('Run with this run number (999) could not be found'),
+        );
+    });
+
+    it('should throw an error if trying to fetch processes for a DPL detector that does not exist', async () => {
+        await assert.rejects(
+            () => dplProcessService.getAllExecutedProcessesByRunAndDetector({ runNumber: 106 }, 999),
+            new NotFoundError('DPL detector with this id could not be found'),
         );
     });
 };
