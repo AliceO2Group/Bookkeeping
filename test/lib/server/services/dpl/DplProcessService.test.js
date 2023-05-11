2b18a4211dfae46dda198b18819f290065c05b4f
Martin Boulais <31805063+martinboulais@users.noreply.github.com>
2023-05-11 15:35:52 +0200

    [O2B-884] Add gRPC endpoint to insert DPL executed processes

--- test/lib/server/services/dpl/DplProcessService.test.js
+++ test/lib/server/services/dpl/DplProcessService.test.js
@@ -16,6 +16,10 @@ const { dplProcessService } = require('../../../../../lib/server/services/dpl/Dp
 const assert = require('assert');
 const { expect } = require('chai');
 const { NotFoundError } = require('../../../../../lib/server/errors/NotFoundError.js');
+const { getDplDetectorOrFail } = require('../../../../../lib/server/services/dpl/getDplDetectorOrFail.js');
+const { getDplProcessOrFail } = require('../../../../../lib/server/services/dpl/getDplProcessOrFail.js');
+const { getDplProcessType } = require('../../../../../lib/server/services/dpl/getDplProcessType.js');
+const { getHostOrFail } = require('../../../../../lib/server/services/host/getHostOrFail.js');
 
 module.exports = () => {
     it('should successfully return the list of detectors that have an executed process for a given run', async () => {
@@ -250,6 +254,128 @@ module.exports = () => {
                 { hostId: 999 },
             ),
             new NotFoundError('Host with this id (999) could not be found'),
+        );
+    });
+
+    it('should successfully create a new process execution and create missing relations if needed', async () => {
+        const runNumber = 104;
+        const processName = 'PROCESS-1';
+        const processTypeLabel = 'QcTask';
+        const detectorName = 'CPV';
+        const hostname = 'FLP-1';
+
+        {
+            const processExecution = await dplProcessService.createProcessExecution(
+                { args: '-v -c config.yml' },
+                {
+                    runIdentifier: { runNumber },
+                    processName,
+                    detectorName,
+                    processTypeLabel,
+                    hostname,
+                },
+            );
+            expect(processExecution).to.be.an('object');
+            expect(processExecution.detectorId).to.equal(1);
+            expect(processExecution.processId).to.equal(1);
+            expect(processExecution.hostId).to.equal(1);
+        }
+
+        {
+            const processExecution = await dplProcessService.createProcessExecution(
+                { args: '-v -c config.yml' },
+                {
+                    runIdentifier: { runNumber },
+                    processName,
+                    detectorName: 'NON-EXISTING-DETECTOR',
+                    processTypeLabel,
+                    hostname,
+                },
+            );
+            expect(processExecution).to.not.be.null;
+            expect(processExecution.detectorId).to.equal(21);
+            const { name } = await getDplDetectorOrFail({ dplDetectorId: 21 });
+            expect(name).to.equal('NON-EXISTING-DETECTOR');
+            expect(processExecution.processId).to.equal(1);
+            expect(processExecution.hostId).to.equal(1);
+        }
+
+        {
+            const processExecution = await dplProcessService.createProcessExecution(
+                { args: '-v -c config.yml' },
+                {
+                    runIdentifier: { runNumber },
+                    detectorName,
+                    processName: 'NON-EXISTING-PROCESS',
+                    processTypeLabel,
+                    hostname,
+                },
+            );
+            expect(processExecution).to.not.be.null;
+            expect(processExecution.detectorId).to.equal(1);
+            expect(processExecution.processId).to.equal(4);
+            const { name, typeId } = await getDplProcessOrFail({ dplProcessId: 4 });
+            expect(name).to.equal('NON-EXISTING-PROCESS');
+            expect(typeId).to.equal(1);
+            expect(processExecution.hostId).to.equal(1);
+        }
+
+        {
+            const processExecution = await dplProcessService.createProcessExecution(
+                { args: '-v -c config.yml' },
+                {
+                    runIdentifier: { runNumber },
+                    processName,
+                    detectorName,
+                    processTypeLabel: 'NON-EXISTING-PROCESS-TYPE',
+                    hostname,
+                },
+            );
+            expect(processExecution).to.not.be.null;
+            expect(processExecution.detectorId).to.equal(1);
+            expect(processExecution.processId).to.equal(5);
+            const { name, typeId } = await getDplProcessOrFail({ dplProcessId: 5 });
+            expect(name).to.equal(processName);
+            expect(typeId).to.equal(7);
+            const type = await getDplProcessType({ dplProcessTypeId: 7 });
+            expect(type).to.not.be.null;
+            expect(type.label).to.equal('NON-EXISTING-PROCESS-TYPE');
+            expect(processExecution.hostId).to.equal(1);
+        }
+
+        {
+            const processExecution = await dplProcessService.createProcessExecution(
+                { args: '-v -c config.yml' },
+                {
+                    runIdentifier: { runNumber },
+                    processName,
+                    detectorName,
+                    processTypeLabel,
+                    hostname: 'NON-EXISTING-HOSTNAME',
+                },
+            );
+            expect(processExecution).to.not.be.null;
+            expect(processExecution.detectorId).to.equal(1);
+            expect(processExecution.processId).to.equal(1);
+            expect(processExecution.hostId).to.equal(4);
+            const { hostname } = await getHostOrFail({ hostId: 4 });
+            expect(hostname).to.equal('NON-EXISTING-HOSTNAME');
+        }
+    });
+
+    it('should throw an error when trying to create a process execution for a non-existing run', async () => {
+        await assert.rejects(
+            () => dplProcessService.createProcessExecution(
+                {},
+                {
+                    runIdentifier: { runNumber: 999 },
+                    detectorName: 'Any',
+                    processName: 'Any',
+                    processTypeLabel: 'Any',
+                    hostname: 'Any',
+                },
+            ),
+            new NotFoundError('Run with this run number (999) could not be found'),
         );
     });
 };
