bd3863a1d1d26a5fc987398a6ccddecbe7cff5ec
Martin Boulais <31805063+martinboulais@users.noreply.github.com>
2023-05-15 16:40:42 +0200

    [O2B-887] Add missing QC tags to EoS report (#994)

--- test/api/tags.test.js
+++ test/api/tags.test.js
@@ -44,7 +44,7 @@ module.exports = () => {
             const response = await request(server).get('/api/tags?filter[partialText]=-TAG-');
             expect(response.status).to.equal(200);
             expect(response.body.data).to.be.an('array');
-            expect(response.body.data.length).to.equal(36);
+            expect(response.body.data.length).to.equal(34);
         });
 
         it('should return 204 if no tags match partial search', async () => {
