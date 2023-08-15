932aeb05063bc10dcbafab438923972a62434940
joetorsney <joetorsney@gmail.com>
2023-08-15 08:51:17 +0200

    try and fix the test again

--- test/public/logs/overview.test.js
+++ test/public/logs/overview.test.js
@@ -507,7 +507,7 @@ module.exports = () => {
         });
         await page.waitForTimeout(400);
         const tableRows = await page.$$('table tr');
-        await page.waitForTimeout(2000);
+        await page.waitForTimeout(5000);
         expect(tableRows.length > 20).to.be.true;
     });
 
