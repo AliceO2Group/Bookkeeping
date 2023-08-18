e4ff77da4bd06001e2c8cb3d8bf333b3e970c780
martinboulais <31805063+martinboulais@users.noreply.github.com>
2023-08-18 12:31:25 +0200

    Add tests

--- test/lib/utilities/index.js
+++ test/lib/utilities/index.js
@@ -12,12 +12,14 @@
  */
 
 const buildUrlTest = require('./buildUrl.test.js');
+const cacheAsyncFunctionTest = require('./cacheAsyncFunction.test.js');
 const deepmerge = require('./deepmerge.test.js');
 const isPromise = require('./isPromise.test.js');
 const stringUtilsTest = require('./stringUtils.test.js');
 
 module.exports = () => {
     describe('buildUrl', buildUrlTest);
+    describe('cacheFunction', cacheAsyncFunctionTest);
     describe('deepmerge', deepmerge);
     describe('isPromise', isPromise);
     describe('stringUtils', stringUtilsTest);
