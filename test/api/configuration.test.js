01b759ce8030d4fe8661df58513257be25b2e4c9
martinboulais <31805063+martinboulais@users.noreply.github.com>
2023-10-10 09:41:02 +0200

    [O2B-913] Add infologger links

---
+++ test/api/configuration.test.js
@@ -0,0 +1,25 @@
+/**
+ *  @license
+ *  Copyright CERN and copyright holders of ALICE O2. This software is
+ *  distributed under the terms of the GNU General Public License v3 (GPL
+ *  Version 3), copied verbatim in the file "COPYING".
+ *
+ *  See http://alice-o2.web.cern.ch/license for full licensing information.
+ *
+ *  In applying this license CERN does not waive the privileges and immunities
+ *  granted to it by virtue of its status as an Intergovernmental Organization
+ *  or submit itself to any jurisdiction.
+ */
+
+const request = require('supertest');
+const { server } = require('../../lib/application.js');
+const { expect } = require('chai');
+
+module.exports = () => {
+    describe('GET /api/configuration', () => {
+        it('should return 200 with the frontend configuration', async () => {
+            const response = await request(server).get('/api/configuration.js');
+            expect(response.status).to.equal(200);
+        });
+    });
+};
