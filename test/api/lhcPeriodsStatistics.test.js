79bc70b75ac608c451b9565e41b86c56a635b534
xsalonx <lukasz.dubiel3@gmail.com>
2023-11-14 12:35:57 +0100

    refactor

--- test/api/lhcPeriodsStatistics.test.js
+++ test/api/lhcPeriodsStatistics.test.js
@@ -272,7 +272,7 @@ module.exports = () => {
                         return;
                     }
 
-                    expect(res.body.errors[0].title).to.equal('Lhc period with this id (9999) could not be found');
+                    expect(res.body.errors[0].title).to.equal('LHC period with this id (9999) could not be found');
                     done();
                 });
         });
