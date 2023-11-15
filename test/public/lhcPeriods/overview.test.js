ae74d4be4f82d2838ad30331f658bfc5cb11b26b
xsalonx <lukasz.dubiel3@gmail.com>
2023-11-15 11:46:38 +0100

    add LhcPeriod overview test

---
+++ test/public/lhcPeriods/overview.test.js
@@ -0,0 +1,126 @@
+/**
+ * @license
+ * Copyright CERN and copyright holders of ALICE O2. This software is
+ * distributed under the terms of the GNU General Public License v3 (GPL
+ * Version 3), copied verbatim in the file "COPYING".
+ *
+ * See http://alice-o2.web.cern.ch/license for full licensing information.
+ *
+ * In applying this license CERN does not waive the privileges and immunities
+ * granted to it by virtue of its status as an Intergovernmental Organization
+ * or submit itself to any jurisdiction.
+ */
+
+const chai = require('chai');
+const {
+    defaultBefore,
+    defaultAfter,
+    goToPage,
+} = require('../defaults');
+
+const { expect } = chai;
+
+const periodNameRegex = /LHC\d\d[a-zA-Z]+/;
+
+module.exports = () => {
+    let page;
+    let browser;
+
+    before(async () => {
+        [page, browser] = await defaultBefore(page, browser);
+        await page.setViewport({
+            width: 700,
+            height: 720,
+            deviceScaleFactor: 1,
+        });
+    });
+
+    after(async () => {
+        [page, browser] = await defaultAfter(page, browser);
+    });
+
+    it('loads the page successfully', async () => {
+        const response = await goToPage(page, 'lhc-period-overview');
+
+        // We expect the page to return the correct status code, making sure the server is running properly
+        expect(response.status()).to.equal(200);
+
+        // We expect the page to return the correct title, making sure there isn't another server running on this port
+        const title = await page.title();
+        expect(title).to.equal('AliceO2 Bookkeeping');
+    });
+
+    it('shows correct datatypes in respective columns', async () => {
+        // Expectations of header texts being of a certain datatype
+        const headerDatatypes = {
+            name: (name) => periodNameRegex.test(name),
+            avgEnergy: (avgEnergy) => !isNaN(avgEnergy),
+        };
+
+        // We find the headers matching the datatype keys
+        const headers = await page.$$('th');
+        const headerIndices = {};
+        for (const [index, header] of headers.entries()) {
+            const headerContent = await page.evaluate((element) => element.id, header);
+            const matchingDatatype = Object.keys(headerDatatypes).find((key) => headerContent === key);
+            if (matchingDatatype !== undefined) {
+                headerIndices[index] = matchingDatatype;
+            }
+        }
+
+        // We expect every value of a header matching a datatype key to actually be of that datatype
+
+        // Use the third row because it is where statistics are present
+        const firstRowCells = await page.$$('tr:nth-of-type(3) td');
+        for (const [index, cell] of firstRowCells.entries()) {
+            if (index in headerIndices) {
+                const cellContent = await page.evaluate((element) => element.innerText, cell);
+                const expectedDatatype = headerDatatypes[headerIndices[index]](cellContent);
+                expect(expectedDatatype).to.be.true;
+            }
+        }
+    });
+
+    it('Should display the correct items counter at the bottom of the page', async () => {
+        await goToPage(page, 'lhc-period-overview');
+        await page.waitForTimeout(100);
+
+        expect(await page.$eval('#firstRowIndex', (element) => parseInt(element.innerText, 10))).to.equal(1);
+        expect(await page.$eval('#lastRowIndex', (element) => parseInt(element.innerText, 10))).to.equal(2);
+        expect(await page.$eval('#totalRowsCount', (element) => parseInt(element.innerText, 10))).to.equal(2);
+    });
+
+    it('can set how many lhcFills are available per page', async () => {
+        await page.waitForTimeout(300);
+        // Expect the amount selector to currently be set to 10 (because of the defined page height)
+        const amountSelectorId = '#amountSelector';
+        const amountSelectorButton = await page.$(`${amountSelectorId} button`);
+        const amountSelectorButtonText = await page.evaluate((element) => element.innerText, amountSelectorButton);
+        await page.waitForTimeout(300);
+        expect(amountSelectorButtonText.trim().endsWith('10')).to.be.true;
+
+        // Expect the dropdown options to be visible when it is selected
+        await amountSelectorButton.evaluate((button) => button.click());
+        await page.waitForTimeout(100);
+        const amountSelectorDropdown = await page.$(`${amountSelectorId} .dropup-menu`);
+        expect(Boolean(amountSelectorDropdown)).to.be.true;
+
+        // Expect the amount of visible lhcfills to reduce when the first option (5) is selected
+        const menuItem = await page.$(`${amountSelectorId} .dropup-menu .menu-item`);
+        await menuItem.evaluate((button) => button.click());
+        await page.waitForTimeout(100);
+
+        const tableRows = await page.$$('table tr');
+        expect(tableRows.length - 1).to.equal(2);
+
+        // Expect the custom per page input to have red border and text color if wrong value typed
+        const customPerPageInput = await page.$(`${amountSelectorId} input[type=number]`);
+        await customPerPageInput.evaluate((input) => input.focus());
+        await page.$eval(`${amountSelectorId} input[type=number]`, (el) => {
+            el.value = '1111';
+            el.dispatchEvent(new Event('input'));
+        });
+        await page.waitForTimeout(100);
+        expect(Boolean(await page.$(`${amountSelectorId} input:invalid`))).to.be.true;
+    });
+};
