0ebb72c3c05fb8e402a41e463125455d3bb9eddb
xsalonx <65893715+xsalonx@users.noreply.github.com>
2023-11-15 10:57:30 +0100

    [O2B-1028] add periods controllers (#1224)
    * [O2B-1025] - lhc periods table extraction (#1193)
    
    * add migration for lhc_periods
    
    * migration and undo works
    
    * amend migration
    
    * amend migration when no data
    
    * add lhc_periods seeders
    
    * add lhcPeriod model
    
    * update Run model
    
    * linter
    
    * amend references in sequelize models
    
    * update filtering rules
    
    * add adapters
    
    * add adapters
    
    * add typedefs
    
    * docs
    
    * rename
    
    * update usecase
    
    * add relation
    
    * refactor
    
    * refactor
    
    * LHC to Lhc
    
    * simplify migration
    
    * utilize lhcPeriodAdapter toDatabase
    
    * simplify filtering
    
    * rename
    
    * update instead of insert
    
    * add lhcPeriod repo
    
    * modify aliases
    
    * add lhcPeriod service method and use in run update
    
    * runAdapter lhcPeriod
    
    * add function for run.lhcPeriod convertion
    
    * adjust run.controller
    
    * adjust dplProcess.controller
    
    * handle null argument in convertRunLhcPeriod
    
    * adjust environment.controller
    
    * add generic stuff
    
    * first utilisation
    
    * handle arrays
    
    * adjust dplProcess.controller
    
    * refactor
    
    * amend dplProcess.controller
    
    * adjust environments.controller
    
    * adjust flps.controller
    
    * adjust lhcFill
    
    * adjust logs.controller
    
    * udjust run controller
    
    * docs
    
    * amend
    
    * amend usecase
    
    * amend inner adapter usage
    
    * docs
    
    * cleanup
    
    * amend test
    
    * amend test
    
    * amend test
    
    * add API adapters
    
    * RunAdapter cleaned
    
    * APIRunAdapter done
    
    * LogAdapter
    
    * ApiLhcFillAdapter
    
    * docs
    
    * docs
    
    * cleanup
    
    * APIHostAdapter
    
    * add common code
    
    * APIFlpRoleAdapter
    
    * APIEnviornmentAdapter
    
    * APIDplProcessAdapter
    
    * APIDplProcessExecutionAdapter
    
    * APIDplDetectorAdapter
    
    * refactor
    
    * refactor
    
    * rename'
    
    * rename
    
    * rename
    
    * init of index
    
    * index
    
    * fulfil dependencies
    
    * remove
    
    * remove imports
    
    * remove rejected stuff
    
    * remove unnecessary adapters
    
    * simplify RunAdapter
    
    * update runs.controller
    
    * update GRPCRun.controller
    
    * cleanup
    
    * revoke some changes
    
    * revoke some changes
    
    * revoke some changes
    
    * simplify
    
    * revoke accidental changes
    
    * rename:
    
    * test refactor
    
    * rename in docs
    
    * refactor
    
    * refactor
    
    * refactor
    
    * update docs
    
    * rename
    
    * refactor
    
    * a
    
    * merge
    
    * refactor
    
    * rename
    
    * cleanup
    
    * rename
    
    * test, rename
    
    * a
    
    * refactor
    
    * test
    
    * add pperiods id to physics runs
    
    * refactor
    
    * amend misuse
    
    * rename
    
    * docs, revoke
    
    * revoke:
    
    * typedefs
    
    * restore
    
    * del
    
    * refactor
    
    * docs
    
    * rename, refactor, test amend
    
    * revoke unwanted cahnge
    
    * init lhcPeriodsStatistics controller
    
    * refactor DtoFactory
    
    * refactor periods controller
    
    * add lhcPeriods router
    
    * expose new router, amend bug
    
    * docs
    
    * docs
    
    * amend Service
    
    * add test
    
    * amend service
    
    * add tests
    
    * rename
    
    * remove unncessary properties
    
    * docs
    
    * refactor
    
    * rename
    
    * refactor
    
    * refactor
    
    * refactor view
    
    * refactor
    
    * refactor model
    
    * refactor
    
    * adapters refactor
    
    * refactor
    
    * amend
    
    * minimfy
    
    * adjust tests
    
    * remove TODO
    
    * deleted dead code
    
    * refactor one test
    
    * adjust test
    
    * add counted data view
    
    * refactor
    
    * docs refactor
    
    * refactor
    
    * working enpoints
    
    * tests
    
    * extend DtoFactory
    
    * refactor, docs
    
    * amend test
    
    * amend service
    
    * add tests
    
    * amend test
    
    * amend test
    
    * amend one method, add one test
    
    * refactor
    
    * docs
    
    * refactor
    
    * refactor, LhcPeriodStatistics bring LhcPeriod
    
    * refactor
    
    * refactor QueryBuilder#whereAssociaiton
    
    * refactor service
    
    * docs
    
    * test
    
    * revoke some changes
    
    * remove page notion
    
    * docs
    
    * amend test
    
    * amend controller pagination

---
+++ test/api/lhcPeriodsStatistics.test.js
@@ -0,0 +1,281 @@
+/**
+ * @license
+ * Copyright CERN and copyright holders of ALICE O2. This software is
+ * distributed under the terms of the GNU General Public License v3 (GPL
+ * Version 3), copied verbatim in the file "COPYING".
+ *
+ * See http://alice-o2.web.cern.ch/license for full licensing information.
+ *
+ * In applying this license CERN does not waive the privileges and immunities
+ * granted to it by virtue of its status as an Intergovernmental Organization
+ * or submit itself to any jurisdiction.
+ */
+
+const { expect } = require('chai');
+const request = require('supertest');
+const { server } = require('../../lib/application');
+const { resetDatabaseContent } = require('../utilities/resetDatabaseContent.js');
+
+const lhcPeriod_LHC22b = {
+    id: 2,
+    avgEnergy: null,
+    lhcPeriod: {
+        id: 2,
+        name: 'LHC22b',
+    },
+};
+
+const lhcPeriod_LHC22a = {
+    id: 1,
+    avgEnergy: 23.209999084472656,
+    lhcPeriod: {
+        id: 1,
+        name: 'LHC22a',
+    },
+};
+
+module.exports = () => {
+    before(resetDatabaseContent);
+
+    describe('GET /api/lhcPeriodsStatistics', () => {
+        it('should successfuly fetch all data', (done) => {
+            request(server)
+                .get('/api/lhcPeriodsStatistics')
+                .expect(200)
+                .end((err, res) => {
+                    if (err) {
+                        done(err);
+                        return;
+                    }
+
+                    const { data, meta } = res.body;
+                    expect(meta).to.be.eql({ page: { totalCount: 2, pageCount: 1 } });
+                    expect(data).to.be.an('array');
+                    expect(data).to.be.lengthOf(2);
+
+                    done();
+                });
+        });
+        it('should successfuly filter on ids', (done) => {
+            request(server)
+                .get('/api/lhcPeriodsStatistics?filter[ids][]=1')
+                .expect(200)
+                .end((err, res) => {
+                    if (err) {
+                        done(err);
+                        return;
+                    }
+
+                    const { data, meta } = res.body;
+                    expect(meta).to.be.eql({ page: { totalCount: 1, pageCount: 1 } });
+                    expect(data).to.be.an('array');
+                    expect(data).to.be.lengthOf(1);
+                    expect(data[0]).to.be.eql(lhcPeriod_LHC22a);
+
+                    done();
+                });
+        });
+        it('should successfuly filter on names', (done) => {
+            request(server)
+                .get('/api/lhcPeriodsStatistics?filter[names][]=LHC22b')
+                .expect(200)
+                .end((err, res) => {
+                    if (err) {
+                        done(err);
+                        return;
+                    }
+
+                    const { data } = res.body;
+                    expect(data).to.be.an('array');
+                    expect(data).to.be.lengthOf(1);
+                    expect(data[0]).to.be.eql(lhcPeriod_LHC22b);
+
+                    done();
+                });
+        });
+        it('should retrive no records when filtering on ids', (done) => {
+            request(server)
+                .get('/api/lhcPeriodsStatistics?filter[ids][]=9999')
+                .expect(200)
+                .end((err, res) => {
+                    if (err) {
+                        done(err);
+                        return;
+                    }
+
+                    const { data } = res.body;
+                    expect(data).to.be.an('array');
+                    expect(data).to.be.lengthOf(0);
+
+                    done();
+                });
+        });
+        it('should retrive no records when filtering on names', (done) => {
+            request(server)
+                .get('/api/lhcPeriodsStatistics?filter[names][]=LHC29xyz')
+                .expect(200)
+                .end((err, res) => {
+                    if (err) {
+                        done(err);
+                        return;
+                    }
+
+                    const { data } = res.body;
+                    expect(data).to.be.an('array');
+                    expect(data).to.be.lengthOf(0);
+                    done();
+                });
+        });
+        it('should succefully filter on ids given as array', (done) => {
+            request(server)
+                .get('/api/lhcPeriodsStatistics?filter[ids][]=1&filter[ids][]=2')
+                .expect(200)
+                .end((err, res) => {
+                    if (err) {
+                        done(err);
+                        return;
+                    }
+
+                    const { data } = res.body;
+                    expect(data).to.be.an('array');
+                    expect(data).to.be.lengthOf(2);
+                    done();
+                });
+        });
+        it('should successfuly sort on id and name', (done) => {
+            request(server)
+                .get('/api/lhcPeriodsStatistics?sort[id]=DESC&sort[name]=ASC')
+                .expect(200)
+                .end((err, res) => {
+                    if (err) {
+                        done(err);
+                        return;
+                    }
+
+                    const { data } = res.body;
+                    expect(data).to.be.an('array');
+                    expect(data).to.be.lengthOf(2);
+                    expect(data[0]).to.be.eql(lhcPeriod_LHC22b);
+                    expect(data[1]).to.be.eql(lhcPeriod_LHC22a);
+
+                    done();
+                });
+        });
+        it('should support pagination', (done) => {
+            request(server)
+                .get('/api/lhcPeriodsStatistics?page[offset]=1&sort[id]=desc')
+                .expect(200)
+                .end((err, res) => {
+                    if (err) {
+                        done(err);
+                        return;
+                    }
+
+                    const { data } = res.body;
+                    expect(data).to.be.an('array');
+                    expect(data).to.be.lengthOf(1);
+                    expect(data[0]).to.be.eql(lhcPeriod_LHC22a);
+
+                    done();
+                });
+        });
+        it('should return 400 when bad query paramter provided', (done) => {
+            request(server)
+                .get('/api/lhcPeriodsStatistics?a=1')
+                .expect(400)
+                .end((err, res) => {
+                    if (err) {
+                        done(err);
+                        return;
+                    }
+
+                    const { errors } = res.body;
+                    const titleError = errors.find((err) => err.source.pointer === '/data/attributes/query/a');
+                    expect(titleError.detail).to.equal('"query.a" is not allowed');
+                    done();
+                });
+        });
+        it('should return 400 if the limit is below 1', (done) => {
+            request(server)
+                .get('/api/lhcPeriodsStatistics?page[limit]=0')
+                .expect(400)
+                .end((err, res) => {
+                    if (err) {
+                        done(err);
+                        return;
+                    }
+
+                    const { errors } = res.body;
+                    const titleError = errors.find((err) => err.source.pointer === '/data/attributes/query/page/limit');
+                    expect(titleError.detail).to.equal('"query.page.limit" must be greater than or equal to 1');
+                    done();
+                });
+        });
+        it('should return 400 if the limit is below 1', (done) => {
+            request(server)
+                .get('/api/lhcPeriodsStatistics?page[limit]=0')
+                .expect(400)
+                .end((err, res) => {
+                    if (err) {
+                        done(err);
+                        return;
+                    }
+
+                    const { errors } = res.body;
+                    const titleError = errors.find((err) => err.source.pointer === '/data/attributes/query/page/limit');
+                    expect(titleError.detail).to.equal('"query.page.limit" must be greater than or equal to 1');
+                    done();
+                });
+        });
+    });
+
+    describe('GET /api/lhcPeriodsStatistics/:lhcPeriodId', () => {
+        it('should successfuly fetch period with given id 1', (done) => {
+            request(server)
+                .get('/api/lhcPeriodsStatistics/1')
+                .expect(200)
+                .end((err, res) => {
+                    if (err) {
+                        done(err);
+                        return;
+                    }
+
+                    const { data } = res.body;
+                    expect(data).to.be.eql(lhcPeriod_LHC22a);
+                    done();
+                });
+        });
+
+        it('should successfuly fetch period with given id 2', (done) => {
+            request(server)
+                .get('/api/lhcPeriodsStatistics/2')
+                .expect(200)
+                .end((err, res) => {
+                    if (err) {
+                        done(err);
+                        return;
+                    }
+
+                    const { data } = res.body;
+                    expect(data).to.be.eql(lhcPeriod_LHC22b);
+                    done();
+                });
+        });
+
+        it('should return 404 if lhc period could not be found', (done) => {
+            request(server)
+                .get('/api/lhcPeriodsStatistics/9999')
+                .expect(404)
+                .end((err, res) => {
+                    if (err) {
+                        done(err);
+                        return;
+                    }
+
+                    expect(res.body.errors[0].title).to.equal('Not found');
+                    expect(res.body.errors[0].detail).to.equal('LHC Period with this id (9999) could not be found');
+                    done();
+                });
+        });
+    });
+};
