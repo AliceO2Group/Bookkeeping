68160f7ecb8f3b1676676a281ff69d5565546451
Sandro Boelsz <97670955+SandroBoelsz@users.noreply.github.com>
2024-01-09 09:07:24 +0100

    [O2B-1091] add comment change to log overview tests

--- test/public/logs/overview.test.js
+++ test/public/logs/overview.test.js
@@ -304,7 +304,7 @@ module.exports = () => {
         await pressElement(page, '#tag-filter-combination-operator-radio-button-or');
         await page.waitForTimeout(300);
 
-        // Expect there now to be more rows than the table with only one filter
+        // Expect there now to be more rows than both the previous table and the table with only one filter
         const thirdFilteredRows = await page.$$('table tr');
         const thirdFilteredNumberOfRows = thirdFilteredRows.length - 1;
         expect(thirdFilteredNumberOfRows).to.be.greaterThan(firstFilteredNumberOfRows);
