ef842677540c91788a47401f3882aa27fc6feca5
xsalonx <lukasz.dubiel3@gmail.com>
2024-02-15 14:22:29 +0100

    refactor

--- test/public/simulationPasses/overviewAnchoredSimulationPasses.test.js
+++ test/public/simulationPasses/overviewAnchoredSimulationPasses.test.js
@@ -36,8 +36,8 @@ module.exports = () => {
         [page, browser] = await defaultAfter(page, browser);
     });
 
-    it('loads the page successfully', async () => {
-        const response = await goToPage(page, 'anchored-simulation-passes-overview', { queryParameters: { dataPassId: 3 } });
+    it('loads page - simulation passes per LHC Period successfully', async () => {
+        const response = await goToPage(page, 'simulation-passes-per-lhc-period-overview', { queryParameters: { lhcPeriodId: 1 } });
 
         // We expect the page to return the correct status code, making sure the server is running properly
         expect(response.status()).to.equal(200);
@@ -45,6 +45,9 @@ module.exports = () => {
         // We expect the page to return the correct title, making sure there isn't another server running on this port
         const title = await page.title();
         expect(title).to.equal('AliceO2 Bookkeeping');
+        const headerBreadcrumbs = await page.$$('h2');
+        expect(await headerBreadcrumbs[0].evaluate((element) => element.innerText)).to.be.equal('Monte Carlo');
+        expect(await headerBreadcrumbs[1].evaluate((element) => element.innerText)).to.be.equal('LHC22a');
     });
 
     it('shows correct datatypes in respective columns', async () => {
@@ -59,6 +62,7 @@ module.exports = () => {
         };
 
         // We find the headers matching the datatype keys
+        await page.waitForSelector('th');
         const headers = await page.$$('th');
         const headerIndices = {};
         for (const [index, header] of headers.entries()) {
@@ -83,78 +87,74 @@ module.exports = () => {
     });
 
     it('Should display the correct items counter at the bottom of the page', async () => {
-        await goToPage(page, 'anchored-simulation-passes-overview', { queryParameters: { dataPassId: 3 } });
-        await page.waitForTimeout(100);
+        await goToPage(page, 'simulation-passes-per-lhc-period-overview', { queryParameters: { lhcPeriodId: 1 } });
+        await page.waitForSelector('#firstRowIndex');
 
         expect(await page.$eval('#firstRowIndex', (element) => parseInt(element.innerText, 10))).to.equal(1);
-        expect(await page.$eval('#lastRowIndex', (element) => parseInt(element.innerText, 10))).to.equal(1);
-        expect(await page.$eval('#totalRowsCount', (element) => parseInt(element.innerText, 10))).to.equal(1);
+        expect(await page.$eval('#lastRowIndex', (element) => parseInt(element.innerText, 10))).to.equal(2);
+        expect(await page.$eval('#totalRowsCount', (element) => parseInt(element.innerText, 10))).to.equal(2);
     });
 
-    it('can set how many data passes is available per page', async () => {
-        await goToPage(page, 'anchored-simulation-passes-overview', { queryParameters: { dataPassId: 3 } });
-        await page.waitForTimeout(500);
+    it('can set how many simulation passes is available per page', async () => {
+        await goToPage(page, 'simulation-passes-per-lhc-period-overview', { queryParameters: { lhcPeriodId: 1 } });
+
         // Expect the amount selector to currently be set to 10 (because of the defined page height)
+        await page.waitForSelector('.dropup button');
         const amountSelectorButton = await page.$('.dropup button');
         const amountSelectorButtonText = await amountSelectorButton.evaluate((element) => element.innerText);
-        await page.waitForTimeout(300);
         expect(amountSelectorButtonText.trim().endsWith('9')).to.be.true;
 
         // Expect the dropdown options to be visible when it is selected
         await amountSelectorButton.evaluate((button) => button.click());
-        await page.waitForTimeout(100);
+        await page.waitForSelector('.dropup');
         const amountSelectorDropdown = await page.$('.dropup');
         expect(Boolean(amountSelectorDropdown)).to.be.true;
 
-        // Expect the amount of visible lhcfills to reduce when the first option (5) is selected
+        // Expect the amount of visible simulationPasses to reduce when the first option (5) is selected
         const menuItem = await page.$('.dropup .menu-item');
         await menuItem.evaluate((button) => button.click());
-        await page.waitForTimeout(100);
-
-        const tableRows = await page.$$('table tr');
-        expect(tableRows.length - 1).to.equal(1);
 
         // Expect the custom per page input to have red border and text color if wrong value typed
         const customPerPageInput = await page.$('.dropup input[type=number]');
         await customPerPageInput.evaluate((input) => input.focus());
-        await page.$eval('.dropup input[type=number]', (el) => {
-            el.value = '1111';
-            el.dispatchEvent(new Event('input'));
+        await page.$eval('.dropup input[type=number]', (element) => {
+            element.value = '1111';
+            element.dispatchEvent(new Event('input'));
         });
-        await page.waitForTimeout(100);
+        await page.waitForSelector('tbody tr');
         expect(Boolean(await page.$('.dropup input:invalid'))).to.be.true;
     });
 
     it('can sort by name column in ascending and descending manners', async () => {
-        await goToPage(page, 'anchored-simulation-passes-overview', { queryParameters: { dataPassId: 3 } });
+        await goToPage(page, 'simulation-passes-per-lhc-period-overview', { queryParameters: { lhcPeriodId: 1 } });
         // Expect a sorting preview to appear when hovering over a column header
+        await page.waitForSelector('th#name');
         await page.hover('th#name');
-        await page.waitForTimeout(100);
         const sortingPreviewIndicator = await page.$('#name-sort-preview');
         expect(Boolean(sortingPreviewIndicator)).to.be.true;
 
         // Sort by name in an ascending manner
         const nameHeader = await page.$('th#name');
         await nameHeader.evaluate((button) => button.click());
-        await page.waitForTimeout(300);
 
+        await page.waitForSelector('tbody tr');
         // Expect the names to be in alphabetical order
         const firstNames = await getAllDataFields(page, 'name');
         expect(firstNames).to.have.all.deep.ordered.members(firstNames.sort());
     });
 
     it('can sort by requestedEventsCount column in ascending and descending manners', async () => {
-        await goToPage(page, 'anchored-simulation-passes-overview', { queryParameters: { dataPassId: 3 } });
+        await goToPage(page, 'simulation-passes-per-lhc-period-overview', { queryParameters: { lhcPeriodId: 1 } });
         // Expect a sorting preview to appear when hovering over a column header
+        await page.waitForSelector('th#requestedEventsCount');
         await page.hover('th#requestedEventsCount');
-        await page.waitForTimeout(100);
         const sortingPreviewIndicator = await page.$('#requestedEventsCount-sort-preview');
         expect(Boolean(sortingPreviewIndicator)).to.be.true;
 
         // Sort by year in an ascending manner
         const requestedEventsCountHeader = await page.$('th#requestedEventsCount');
         await requestedEventsCountHeader.evaluate((button) => button.click());
-        await page.waitForTimeout(300);
+        await page.waitForSelector('tbody tr');
 
         // Expect the year to be in order
         const firstReconstructedEventsCounts = await getAllDataFields(page, 'requestedEventsCount');
@@ -162,17 +162,17 @@ module.exports = () => {
     });
 
     it('can sort by generatedEventsCount column in ascending and descending manners', async () => {
-        await goToPage(page, 'anchored-simulation-passes-overview', { queryParameters: { dataPassId: 3 } });
+        await goToPage(page, 'simulation-passes-per-lhc-period-overview', { queryParameters: { lhcPeriodId: 1 } });
         // Expect a sorting preview to appear when hovering over a column header
+        await page.waitForSelector('th#generatedEventsCount');
         await page.hover('th#generatedEventsCount');
-        await page.waitForTimeout(100);
         const sortingPreviewIndicator = await page.$('#generatedEventsCount-sort-preview');
         expect(Boolean(sortingPreviewIndicator)).to.be.true;
 
         // Sort by year in an ascending manner
         const generatedEventsCountHeader = await page.$('th#generatedEventsCount');
         await generatedEventsCountHeader.evaluate((button) => button.click());
-        await page.waitForTimeout(300);
+        await page.waitForSelector('tbody tr');
 
         // Expect the year to be in order
         const firstReconstructedEventsCounts = await getAllDataFields(page, 'generatedEventsCount');
@@ -180,55 +180,40 @@ module.exports = () => {
     });
 
     it('can sort by outputSize column in ascending and descending manners', async () => {
-        await goToPage(page, 'anchored-simulation-passes-overview', { queryParameters: { dataPassId: 3 } });
+        await goToPage(page, 'simulation-passes-per-lhc-period-overview', { queryParameters: { lhcPeriodId: 1 } });
         // Expect a sorting preview to appear when hovering over a column header
+        await page.waitForSelector('th#outputSize');
         await page.hover('th#outputSize');
-        await page.waitForTimeout(100);
         const sortingPreviewIndicator = await page.$('#outputSize-sort-preview');
         expect(Boolean(sortingPreviewIndicator)).to.be.true;
 
         // Sort by avgCenterOfMassEnergy in an ascending manner
         const outputSizeHeader = await page.$('th#outputSize');
         await outputSizeHeader.evaluate((button) => button.click());
-        await page.waitForTimeout(300);
+        await page.waitForSelector('tbody tr');
 
         // Expect the avgCenterOfMassEnergy to be in order
         const firstOutputSize = await getAllDataFields(page, 'outputSize');
         expect(firstOutputSize).to.have.all.deep.ordered.members(firstOutputSize.sort());
     });
 
-    it('should successfuly apply simulation pass name filter', async () => {
-        await goToPage(page, 'anchored-simulation-passes-overview', { queryParameters: { dataPassId: 3 } });
-        await page.waitForTimeout(100);
+    it('should successfuly apply simulation passes name filter', async () => {
+        await goToPage(page, 'simulation-passes-per-lhc-period-overview', { queryParameters: { lhcPeriodId: 1 } });
+        await page.waitForSelector('#openFilterToggle');
         const filterToggleButton = await page.$('#openFilterToggle');
         expect(filterToggleButton).to.not.be.null;
 
         await filterToggleButton.evaluate((button) => button.click());
-        await fillInput(page, 'div.flex-row.items-baseline:nth-of-type(2) input[type=text]', 'LHC23k6b');
-
-        await page.waitForTimeout(100);
-
-        /**
-         * As @see getAllDataFields returns innerText from cells, in case of simulationPass.name column, text from inner buttons is also taken.
-         * @param {string[]} simulationPassName list of names
-         * @return {string[]} cells content
-         */
-        const appendButtonsText = (simulationPassName) => simulationPassName.map((p) => `${p}`);
+        await fillInput(page, 'div.flex-row.items-baseline:nth-of-type(2) input[type=text]', 'LHC23k6a');
+        await page.waitForSelector('tbody tr');
 
         let allDataPassesNames = await getAllDataFields(page, 'name');
-        expect(allDataPassesNames).to.has.all.deep.members(appendButtonsText(['LHC23k6b']));
+        expect(allDataPassesNames).to.has.all.deep.members(['LHC23k6a']);
 
-        await fillInput(page, 'div.flex-row.items-baseline:nth-of-type(2) input[type=text]', 'LHC23k6c');
-        await page.waitForTimeout(100);
-        allDataPassesNames = await getAllDataFields(page, 'name');
-        expect(allDataPassesNames).to.be.lengthOf(0);
-
-        const resetFiltersButton = await page.$('#reset-filters');
-        expect(resetFiltersButton).to.not.be.null;
-        await resetFiltersButton.evaluate((button) => button.click());
-        await page.waitForTimeout(200);
+        await fillInput(page, 'div.flex-row.items-baseline:nth-of-type(2) input[type=text]', 'LHC23k6a, LHC23k6b');
+        await page.waitForSelector('tbody tr');
 
         allDataPassesNames = await getAllDataFields(page, 'name');
-        expect(allDataPassesNames).to.has.all.deep.members(appendButtonsText(['LHC23k6b']));
+        expect(allDataPassesNames).to.has.all.deep.members(['LHC23k6a', 'LHC23k6b']);
     });
 };
