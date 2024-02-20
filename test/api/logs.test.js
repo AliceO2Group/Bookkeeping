5347a94c4227a9872dab6db332132b305ccb0b87
Anwar Hamdi <anwar.hamdi@hva.nl>
2024-02-20 12:38:00 +0100

    O2B-904: Implemented all changes requested

--- test/api/logs.test.js
+++ test/api/logs.test.js
@@ -775,7 +775,7 @@ module.exports = () => {
     });
 
     describe('GET /api/logs/root', () => {
-        it('should return root logs only', (done) => {
+        it('should return root logs only, ensuring no log has a parent', (done) => {
             request(server)
                 .get('/api/logs/root')
                 .expect(200)
@@ -791,7 +791,7 @@ module.exports = () => {
                 });
         });
 
-        it('should return an array', (done) => {
+        it('should return root logs as an array', (done) => {
             request(server)
                 .get('/api/logs/root')
                 .expect(200)
@@ -801,19 +801,32 @@ module.exports = () => {
                         return;
                     }
 
-                    expect(res.body.data).to.be.an('array');
+                    expect(Array.isArray(res.body.data)).to.be.true;
 
                     done();
                 });
         });
 
-        it('should return 400 if the limit is below 1', async () => {
+        it('should reject requests with a limit below 1 with a 400 status', async () => {
             const response = await request(server).get('/api/logs/root?page[offset]=0&page[limit]=0');
             expect(response.status).to.equal(400);
 
             const { errors } = response.body;
             const titleError = errors.find((err) => err.source.pointer === '/data/attributes/query/page/limit');
             expect(titleError.detail).to.equal('"query.page.limit" must be greater than or equal to 1');
+        });
+
+        it('should successfully return paginated root logs based on specified limit and offset', async () => {
+            const limit = 2;
+            const offset = 0;
+            const response = await request(server).get(`/api/logs/root?page[offset]=${offset}&page[limit]=${limit}`);
+
+            expect(response.status).to.equal(200);
+            expect(response.body.data.length).to.equal(limit);
+
+            const expectedLogIds = [1, 5];
+            const receivedLogIds = response.body.data.map((log) => log.id);
+            expect(receivedLogIds).to.deep.equal(expectedLogIds);
         });
     });
 
