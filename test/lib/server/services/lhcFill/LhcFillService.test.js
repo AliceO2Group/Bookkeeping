33ae3ae2828a58120c31129d81d08968c572b7d2
martinboulais <31805063+martinboulais@users.noreply.github.com>
2024-02-21 18:45:46 +0100

    Add statistics

--- test/lib/server/services/lhcFill/LhcFillService.test.js
+++ test/lib/server/services/lhcFill/LhcFillService.test.js
@@ -38,7 +38,7 @@ module.exports = () => {
         expect(lhcFill.fillNumber).to.equal(6);
     });
 
-    it('should successfully return the list of fills ended in a given period', async () => {
+    it('should successfully return the list of fills ended in a given period including its statistics', async () => {
         // For the purpose of the test, create 2 fills with fixed creation date
         const firstCreatedAt = new Date('2019-08-09 18:00:00');
         const secondCreatedAt = new Date('2019-08-09 20:00:00');
@@ -50,6 +50,8 @@ module.exports = () => {
             });
             expect(lhcFills).to.lengthOf(2);
             expect(lhcFills.map(({ fillNumber }) => fillNumber)).to.have.members([2, 3]);
+            expect(lhcFills[0].statistics).to.be.an('object');
+            expect(lhcFills[1].statistics).to.be.an('object');
         }
 
         {
@@ -59,6 +61,7 @@ module.exports = () => {
             });
             expect(lhcFills).to.lengthOf(1);
             expect(lhcFills[0].fillNumber).to.equal(3);
+            expect(lhcFills[0].statistics).to.be.an('object');
         }
 
         {
@@ -68,6 +71,9 @@ module.exports = () => {
             });
             expect(lhcFills).to.lengthOf(3);
             expect(lhcFills.map(({ fillNumber }) => fillNumber)).to.have.members([2, 3, 4]);
+            expect(lhcFills[0].statistics).to.be.an('object');
+            expect(lhcFills[1].statistics).to.be.an('object');
+            // No statistics for 4th fill which has no stable beam
         }
     });
 };
