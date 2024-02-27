e0182642b71cd0053284bf126eb842cf5c92bfa7
xsalonx <lukasz.dubiel3@gmail.com>
2024-02-27 12:28:19 +0100

    refactor

--- test/public/defaults.js
+++ test/public/defaults.js
@@ -441,15 +441,12 @@ module.exports.checkMismatchingUrlParam = async (page, expectedUrlParameters) =>
 };
 
 /**
- * Wait for table to be reloaded.
  * Call a trigger function, wait for the table to display a loading spinner then wait for the loading spinner to be removed.
  * @param {puppeteer.Page} page the puppeteer page
  * @param {function} triggerFunction function called to trigger table data loading
- * @param {boolean} [expectDataAfterReload = true] if true it will wait until table with data is rendered
- * (so at least one row with data MUST be rendered), otherwise it does not wait for table with new data
  * @return {Promise} promise
  */
-module.exports.waitForTableDataReload = (page, triggerFunction, expectDataAfterReload = true) => Promise.all([
+module.exports.waitForTableDataReload = (page, triggerFunction) => Promise.all([
     page.waitForSelector('table .atom-spinner'),
     triggerFunction(),
-]).then(() => expectDataAfterReload ? page.waitForSelector('tbody tr td:nth-child(2)') : null);
+]).then(() => page.waitForSelector('table .atom-spinner', { hidden: true }));
