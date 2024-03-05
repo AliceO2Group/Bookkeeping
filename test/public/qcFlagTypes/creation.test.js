ea0809b69ff2670ece21b7608344df3fe5b1c5c6
xsalonx <lukasz.dubiel3@gmail.com>
2024-03-05 16:06:02 +0100

    add test

---
+++ test/public/qcFlagTypes/creation.test.js
@@ -0,0 +1,51 @@
+/**
+ * @license
+ * Copyright CERN and copyright holders of ALICE O2. This software is
+ * distributed under the terms of the GNU General Public License v3 (GPL
+ * Version 3), copied verbatim in the file "COPYING".
+ *
+ * See http://alice-o2.web.cern.ch/license for full licensing information.
+ *
+ * In applying this license CERN does not waive the privileges and immunities
+ * granted to it by virtue of its status as an Intergovernmental Organization
+ * or submit itself to any jurisdiction.
+ */
+
+const chai = require('chai');
+const {
+    defaultBefore,
+    defaultAfter,
+    goToPage,
+    testTableAscendingSortingByColumn,
+    waitForTableDataReload,
+    fillInput,
+    getAllDataFields,
+} = require('../defaults');
+
+const { expect } = chai;
+
+module.exports = () => {
+    let page;
+    let browser;
+
+    before(async () => {
+        [page, browser] = await defaultBefore(page, browser);
+    });
+
+    after(async () => {
+        [page, browser] = await defaultAfter(page, browser);
+    });
+
+    it('loads page - QC Flag Types overview successfully', async () => {
+        const response = await goToPage(page, 'qc-flag-type-creation');
+
+        // We expect the page to return the correct status code, making sure the server is running properly
+        expect(response.status()).to.equal(200);
+
+        // We expect the page to return the correct title, making sure there isn't another server running on this port
+        const title = await page.title();
+        expect(title).to.equal('AliceO2 Bookkeeping');
+        const header = await page.$('h2');
+        expect(await header.evaluate((element) => element.innerText)).to.be.equal('QC Flag Type Creation');
+    });
+};
