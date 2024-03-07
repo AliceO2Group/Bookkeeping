1c338339fc1c6ac3bd26f7ac88b5ebe3f446ca9e
xsalonx <lukasz.dubiel3@gmail.com>
2024-03-07 11:13:45 +0100

    add timeout

--- test/utilities/waitForDownload.js
+++ test/utilities/waitForDownload.js
@@ -14,10 +14,11 @@
 /**
  * Create promise which is resolved when last initiated download is completed and rejected when canceled
  * @param {CDPSession} session puppetear CDP session
+ * @param {number} [timeout = 10000] download timeout
  * @return {Promise} promise
  * !!! Downloading requires to set 'Browser.setDownloadBehavior' behaviour on the given CDP session
  */
-async function waitForDownload(session) {
+async function waitForDownload(session, timeout = 10000) {
     return new Promise((resolve, reject) => {
         session.on('Browser.downloadProgress', (event) => {
             if (event.state === 'completed') {
@@ -26,6 +27,7 @@ async function waitForDownload(session) {
                 reject('download canceled');
             }
         });
+        setTimeout(() => reject('Timeout for download'), timeout);
     });
 }
 
