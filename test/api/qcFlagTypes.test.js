2d11b3eaed2fcd6c1c8b4a547ae50bef07db31db
xsalonx <lukasz.dubiel3@gmail.com>
2024-03-28 14:09:14 +0100

    cleanup

--- test/api/qcFlagTypes.test.js
+++ test/api/qcFlagTypes.test.js
@@ -437,7 +437,6 @@ module.exports = () => {
                     }
 
                     const { data: newQCFlag } = res.body;
-                    console.log(newQCFlag, 'TOBEC')
                     expectObjectToBeSuperset(newQCFlag, parameters);
                     done();
                 });
