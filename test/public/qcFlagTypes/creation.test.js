d8440676034cf65e33a1b88286e99891646ac7a4
xsalonx <65893715+xsalonx@users.noreply.github.com>
2024-04-05 08:25:06 +0200

    [O2B-1186] Add QC flag type creation page (#1447)
    * add addociation name
    
    * add controller
    
    * add router
    
    * add repo
    
    * add repo
    
    * get all reasons service function
    
    * add test
    
    * add ep,; test WIP
    
    * add provenenace
    
    * revoke
    
    * test WIP
    
    * t WIP
    
    * test
    
    * WIP
    
    * simplify test
    
    * wIP
    
    * WIP
    
    * change error messages
    
    * qcf creation test ok
    
    * add coalesc comment to null
    
    * expose ep
    
    * a
    
    * add test ok
    
    * typo
    
    * typo
    
    * miss
    
    * fixed
    
    * revoke
    
    * rm verifications
    
    * cherry-pick
    
    * name archived
    
    * obsolate to archived
    
    * simplify association
    
    * a
    
    * Revert "a"
    
    This reverts commit a7b880c5b140d096e8c66ff8079800203628a1c6.
    
    * Revert "simplify association"
    
    This reverts commit ad453da5abd365366090dc7b0ef75496513b2c6e.
    
    * docs
    
    * rename
    
    * simplify
    
    * refactor
    
    * refactor
    
    * refactor
    
    * a
    
    * add assoc
    
    * rename
    
    * cleanup
    
    * fix associations
    
    * comment
    
    * rename table
    
    * reason to type
    
    * rename
    
    * rename
    
    * docs
    
    * cleanup
    
    * refactor
    
    * rename
    
    * fix
    
    * WIP
    
    * typo
    
    * fix
    
    * WIP
    
    * typo
    
    * rename
    
    * serv fixed
    
    * seeders
    
    * fix Test
    
    * simp
    
    * docs
    
    * WIP
    
    * add test
    
    * rename
    
    * update
    
    * fix test
    
    * wip
    
    * clean
    
    * clean
    
    * cleanup
    
    * add modification control
    
    * update modification control
    
    * update associatioin name
    
    * update
    
    * test ok
    
    * test ok
    
    * docs
    
    * add color
    
    * add color
    
    * test ok
    
    * add assoc name
    
    * add seeder
    
    * cherry-pick
    
    * cleanup
    
    * cleanup
    
    * use QC
    
    * restore
    
    * fix
    
    * use QC
    
    * abbr
    
    * abbr
    
    * abbr
    
    * exposed
    
    * amend endpoint
    
    * a
    
    * add columns fro bad
    
    * docs
    
    * fetch works
    
    * filtering works
    
    * works
    
    * test
    
    * test ok
    
    * new test
    
    * test ok
    
    * wip
    
    * rename
    
    * test
    
    * expose filtering
    
    * new filter
    
    * new filter
    
    * amend
    
    * test
    
    * docs
    
    * amend
    
    * works
    
    * test
    
    * tyop
    
    * add handler
    
    * a
    
    * add test
    
    * test
    
    * test ok
    
    * test
    
    * ok
    
    * add model
    
    * WIP
    
    * page works
    
    * WIP
    
    * one create
    
    * update rows number
    
    * rename
    
    * Rename
    
    * add test
    
    * test IWP
    
    * refactor
    
    * tests ok
    
    * checking color
    
    * update errors
    
    * update
    
    * add color default
    
    * cleanup tests
    
    * simplify
    
    * simp
    
    * add metrhod
    
    * add test
    
    * api test:
    
    * controller
    
    * WIP
    
    * test
    
    * docs
    
    * docs;
    
    * Update lib/server/services/qualityControlFlag/QcFlagTypeService.js
    
    Co-authored-by: Martin Boulais <31805063+martinboulais@users.noreply.github.com>
    
    * Update lib/server/services/qualityControlFlag/QcFlagTypeService.js
    
    Co-authored-by: Martin Boulais <31805063+martinboulais@users.noreply.github.com>
    
    * Update lib/server/services/qualityControlFlag/QcFlagTypeService.js
    
    Co-authored-by: Martin Boulais <31805063+martinboulais@users.noreply.github.com>
    
    * Update lib/server/services/qualityControlFlag/QcFlagTypeService.js
    
    Co-authored-by: Martin Boulais <31805063+martinboulais@users.noreply.github.com>
    
    * Update lib/server/services/qualityControlFlag/QcFlagTypeService.js
    
    Co-authored-by: Martin Boulais <31805063+martinboulais@users.noreply.github.com>
    
    * fix
    
    * correct order
    
    * a
    
    * cleanup
    
    * prune
    
    * amend
    
    * cleanup|
    
    * test
    
    * add dependency
    
    * cleanup
    
    * docs
    
    * a
    
    * amend
    
    * test
    
    * amend
    
    * more spec
    
    * cleanup
    
    * amend test
    
    * add rbac
    
    * tesT
    
    * docs
    
    * cleanup
    
    * fix test
    
    * use sessionService
    
    * rename
    
    * simp
    
    * simp
    
    ---------
    
    Co-authored-by: Martin Boulais <31805063+martinboulais@users.noreply.github.com>

---
+++ test/public/qcFlagTypes/creation.test.js
@@ -0,0 +1,80 @@
+/**
+ * @license
+ * Copyright CERN and copyright holders of ALICE O2. This software is
+ * distributed under the terms of the GNU General Public License v3 (GPL
+ * Version 3), copied verbatim in the file "COPYING".
+ *
+ * See http://alice-o2.web.cern.ch/license for full licensing information.
+ *
+ * In applying this license CERN does not waive the privileges and immunities
+ * granted to it by virtue of its status as an Intergovernmental Organization
+ * or submit itself to any jurisdiction.
+ */
+
+const chai = require('chai');
+const {
+    defaultBefore,
+    defaultAfter,
+    goToPage,
+    fillInput,
+    pressElement,
+    expectInnerText,
+    waitForNavigation,
+    validateElement,
+    checkMismatchingUrlParam,
+} = require('../defaults');
+
+const { expect } = chai;
+
+module.exports = () => {
+    let page;
+    let browser;
+
+    before(async () => {
+        [page, browser] = await defaultBefore(page, browser);
+    });
+
+    after(async () => {
+        [page, browser] = await defaultAfter(page, browser);
+    });
+
+    it('loads page - QC Flag Types overview successfully', async () => {
+        const response = await goToPage(page, 'qc-flag-type-creation');
+        expect(response.status()).to.equal(200);
+
+        const title = await page.title();
+        expect(title).to.equal('AliceO2 Bookkeeping');
+        await expectInnerText(page, 'h2', 'QC Flag Type Creation');
+    });
+
+    it('should fail if attempt to create QC Flag Type with already existing name', async () => {
+        await goToPage(page, 'qc-flag-type-creation');
+        await validateElement(page, 'button#submit[disabled]');
+
+        await fillInput(page, 'input#name', 'LimitedAcceptance');
+        await fillInput(page, 'input#method', 'Limited acceptance');
+        await pressElement(page, 'button#submit');
+        await expectInnerText(
+            page,
+            '.alert.alert-danger',
+            'The request conflicts with existing data: A QC flag with name LimitedAcceptance or Limited acceptance already exists',
+        );
+    });
+
+    it('should succesfully create QC Flag Type', async () => {
+        await goToPage(page, 'qc-flag-type-creation');
+        await validateElement(page, 'button#submit[disabled]');
+
+        await fillInput(page, 'input#name', 'AAA+');
+        await fillInput(page, 'input#method', 'A+A+A');
+        await fillInput(page, 'input[type=color]', '#F000F0');
+
+        await page.waitForSelector('button#submit[disabled]', { hidden: true, timeout: 250 });
+
+        await waitForNavigation(page, () => pressElement(page, 'button#submit'));
+        expect(await checkMismatchingUrlParam(page, { page: 'qc-flag-types-overview' })).to.be.eql({});
+
+        // Expect newly created flag to appear and have correct color
+        await expectInnerText(page, '[style*="rgb(240, 0, 240)"]', 'AAA+');
+    });
+};
