f667bff06dfb3ab64a37003557385290f1d7a4c8
xsalonx <lukasz.dubiel3@gmail.com>
2024-04-10 15:30:21 +0200

    rename

--- test/lib/server/services/qualityControlFlag/QcFlagService.test.js
+++ test/lib/server/services/qualityControlFlag/QcFlagService.test.js
@@ -255,7 +255,7 @@ module.exports = () => {
             );
         });
 
-        it('should fail to create quality control flag due to qc flag time period out of range of run\'s period', async () => {
+        it('should fail to create quality control flag due to qc flag `from` timestamp is smaller than run.startTime', async () => {
             const qcFlagCreationParameters = {
                 from: new Date('2019-08-08 11:36:40').getTime(), // Failing property
                 to: new Date('2019-08-09 05:40:00').getTime(),
@@ -279,7 +279,7 @@ module.exports = () => {
             );
         });
 
-        it('should fail to create quality control flag due to qc flag time period out of range of run\'s period', async () => {
+        it('should fail to create quality control flag due to qc flag `to` timestamp is greater than run.stopTime', async () => {
             const qcFlagCreationParameters = {
                 from: new Date('2019-08-09 04:16:40').getTime(), // Failing property
                 to: new Date('2019-08-08 21:20:00').getTime(), // Failing property
