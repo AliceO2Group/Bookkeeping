13a87d2a2ea3dd7df30fc7cca5bdcb21a12578c2
xsalonx <lukasz.dubiel3@gmail.com>
2024-04-15 12:00:33 +0200

    test

--- test/api/qcFlags.test.js
+++ test/api/qcFlags.test.js
@@ -441,7 +441,7 @@ module.exports = () => {
     });
 
     describe('POST /api/qcFlags/:id/verify', () => {
-        it('should fail to verify QC flag of dataPass when being owner', async () => {
+        it('should fail to verify QC flag when being owner', async () => {
             const qcFlagCreationParameters = {
                 from: new Date('2019-08-09 01:29:50').getTime(),
                 to: new Date('2019-08-09 05:40:00').getTime(),
