3ad928e2c65e7757277fa5c99f2cd9427ffe365e
martinboulais <31805063+martinboulais@users.noreply.github.com>
2024-04-16 17:47:23 +0200

    Fix tests

--- test/api/qcFlags.test.js
+++ test/api/qcFlags.test.js
@@ -64,8 +64,8 @@ module.exports = () => {
         });
     });
 
-    describe('GET /api/qcFlags', () => {
-        it('should successfully fecth QC flags for data passes', async () => {
+    describe('GET /api/qcFlags/perDataPass and /api/qcFlags/perSimulationPass', () => {
+        it('should successfully fetch QC flags for data pass', async () => {
             const response = await request(server)
                 .get('/api/qcFlags/perDataPass?dataPassId=1&runNumber=106&dplDetectorId=1');
             expect(response.status).to.be.equal(200);
@@ -73,187 +73,53 @@ module.exports = () => {
             expect(meta).to.be.eql({ page: { totalCount: 3, pageCount: 1 } });
             expect(data).to.be.an('array');
             expect(data).to.be.lengthOf(3);
-            expect(data.map(({ id }) => id)).to.have.all.members([1, 2, 3]);
+            expect(data.map(({ qcFlagId }) => qcFlagId)).to.have.all.members([1, 2, 3]);
         });
 
-        it('should successfuly filter on simulationPassIds, runNumbers, dplDetectorIds', async () => {
+        it('should successfully fetch QC flags for simulation pass', async () => {
             const response = await request(server)
-                .get('/api/qcFlags?filter[simulationPassIds][]=1&filter[runNumbers][]=106&filter[dplDetectorIds][]=1');
+                .get('/api/qcFlags/perSimulationPass?simulationPassId=1&runNumber=106&dplDetectorId=1');
             expect(response.status).to.be.equal(200);
             const { data, meta } = response.body;
             expect(meta).to.be.eql({ page: { totalCount: 1, pageCount: 1 } });
             expect(data).to.be.an('array');
             expect(data).to.be.lengthOf(1);
-            expect(data[0].id).to.be.equal(5);
-        });
-
-        it('should retrive no records when filtering on non-existing ids', async () => {
-            const response = await request(server).get('/api/qcFlags?filter[ids][]=9999');
-            expect(response.status).to.be.equal(200);
-            const { data } = response.body;
-            expect(data).to.be.an('array');
-            expect(data).to.be.lengthOf(0);
-        });
-        it('should successfuly filter on createdBy', async () => {
-            const response = await request(server).get('/api/qcFlags?filter[createdBy][]=John%20Doe');
-            expect(response.status).to.be.equal(200);
-            const { data } = response.body;
-            expect(data).to.be.an('array');
-            expect(data).to.be.lengthOf(3);
-            expect(data.map(({ id }) => id)).to.have.all.members([1, 2, 3]);
-        });
-        it('should support sorting by id', async () => {
-            const response = await request(server).get('/api/qcFlags?sort[id]=ASC');
-            const { data: flags, meta: { page: { totalCount: count } } } = response.body;
-            expect(count).to.be.equal(5);
-            expect(flags).to.be.an('array');
-            expect(flags).to.be.lengthOf(5);
-            const fetchedSortedProperties = flags.map(({ id }) => id);
-            expect(fetchedSortedProperties).to.have.all.ordered.members([...fetchedSortedProperties].sort());
-        });
-
-        // Sorting in ASC order
-        it('should support sorting by `from` property', async () => {
-            const response = await request(server).get('/api/qcFlags?sort[from]=ASC');
-            const { data: flags, meta: { page: { totalCount: count } } } = response.body;
-            expect(count).to.be.equal(5);
-            expect(flags).to.be.an('array');
-            expect(flags).to.be.lengthOf(5);
-            const fetchedSortedProperties = flags.map(({ from }) => from);
-            expect(fetchedSortedProperties).to.have.all.ordered.members([...fetchedSortedProperties].sort());
-        });
-
-        it('should support sorting by `to` property', async () => {
-            const response = await request(server).get('/api/qcFlags?sort[to]=ASC');
-            const { data: flags, meta: { page: { totalCount: count } } } = response.body;
-            expect(count).to.be.equal(5);
-            expect(flags).to.be.an('array');
-            expect(flags).to.be.lengthOf(5);
-            const fetchedSortedProperties = flags.map(({ to }) => to);
-            expect(fetchedSortedProperties).to.have.all.ordered.members([...fetchedSortedProperties].sort());
-        });
-
-        it('should support sorting by flag type name', async () => {
-            const response = await request(server).get('/api/qcFlags?sort[flagType]=ASC');
-            const { data: flags, meta: { page: { totalCount: count } } } = response.body;
-            expect(count).to.be.equal(5);
-            expect(flags).to.be.an('array');
-            expect(flags).to.be.lengthOf(5);
-            const fetchedSortedProperties = flags.map(({ flagType: { name } }) => name);
-            expect(fetchedSortedProperties).to.have.all.ordered.members([...fetchedSortedProperties].sort());
-        });
-
-        it('should support sorting by createdBy name', async () => {
-            const response = await request(server).get('/api/qcFlags?sort[createdBy]=ASC');
-            const { data: flags, meta: { page: { totalCount: count } } } = response.body;
-            expect(count).to.be.equal(5);
-            expect(flags).to.be.an('array');
-            expect(flags).to.be.lengthOf(5);
-            const fetchedSortedProperties = flags.map(({ createdBy: { name } }) => name);
-            expect(fetchedSortedProperties).to.have.all.ordered.members([...fetchedSortedProperties].sort());
-        });
-
-        it('should support sorting by createdAt', async () => {
-            const response = await request(server).get('/api/qcFlags?sort[createdAt]=ASC');
-            const { data: flags, meta: { page: { totalCount: count } } } = response.body;
-            expect(count).to.be.equal(5);
-            expect(flags).to.be.an('array');
-            expect(flags).to.be.lengthOf(5);
-            const fetchedSortedProperties = flags.map(({ createdAt }) => createdAt);
-            expect(fetchedSortedProperties).to.have.all.ordered.members([...fetchedSortedProperties].sort());
-        });
-
-        it('should support sorting by updatedAt', async () => {
-            const response = await request(server).get('/api/qcFlags?sort[updatedAt]=ASC');
-            const { data: flags, meta: { page: { totalCount: count } } } = response.body;
-            expect(count).to.be.equal(5);
-            expect(flags).to.be.an('array');
-            expect(flags).to.be.lengthOf(5);
-            const fetchedSortedProperties = flags.map(({ updatedAt }) => updatedAt);
-            expect(fetchedSortedProperties).to.have.all.ordered.members([...fetchedSortedProperties].sort());
-        });
-
-        // Sorting in DESC order
-        it('should support sorting by `from` property', async () => {
-            const response = await request(server).get('/api/qcFlags?sort[from]=DESC');
-            const { data: flags, meta: { page: { totalCount: count } } } = response.body;
-            expect(count).to.be.equal(5);
-            expect(flags).to.be.an('array');
-            expect(flags).to.be.lengthOf(5);
-            const fetchedSortedProperties = flags.map(({ from }) => from);
-            expect(fetchedSortedProperties).to.have.all.ordered.members([...fetchedSortedProperties].sort().reverse());
-        });
-
-        it('should support sorting by `to` property', async () => {
-            const response = await request(server).get('/api/qcFlags?sort[to]=DESC');
-            const { data: flags, meta: { page: { totalCount: count } } } = response.body;
-            expect(count).to.be.equal(5);
-            expect(flags).to.be.an('array');
-            expect(flags).to.be.lengthOf(5);
-            const fetchedSortedProperties = flags.map(({ to }) => to);
-            expect(fetchedSortedProperties).to.have.all.ordered.members([...fetchedSortedProperties].sort().reverse());
-        });
-
-        it('should support sorting by flag type name', async () => {
-            const response = await request(server).get('/api/qcFlags?sort[flagType]=DESC');
-            const { data: flags, meta: { page: { totalCount: count } } } = response.body;
-            expect(count).to.be.equal(5);
-            expect(flags).to.be.an('array');
-            expect(flags).to.be.lengthOf(5);
-            const fetchedSortedProperties = flags.map(({ flagType: { name } }) => name);
-            expect(fetchedSortedProperties).to.have.all.ordered.members([...fetchedSortedProperties].sort().reverse());
-        });
-
-        it('should support sorting by createdBy name', async () => {
-            const response = await request(server).get('/api/qcFlags?sort[createdBy]=DESC');
-            const { data: flags, meta: { page: { totalCount: count } } } = response.body;
-            expect(count).to.be.equal(5);
-            expect(flags).to.be.an('array');
-            expect(flags).to.be.lengthOf(5);
-            const fetchedSortedProperties = flags.map(({ createdBy: { name } }) => name);
-            expect(fetchedSortedProperties).to.have.all.ordered.members([...fetchedSortedProperties].sort().reverse());
-        });
-
-        it('should support sorting by createdAt', async () => {
-            const response = await request(server).get('/api/qcFlags?sort[createdAt]=DESC');
-            const { data: flags, meta: { page: { totalCount: count } } } = response.body;
-            expect(count).to.be.equal(5);
-            expect(flags).to.be.an('array');
-            expect(flags).to.be.lengthOf(5);
-            const fetchedSortedProperties = flags.map(({ createdAt }) => createdAt);
-            expect(fetchedSortedProperties).to.have.all.ordered.members([...fetchedSortedProperties].sort().reverse());
-        });
-
-        it('should support sorting by updatedAt', async () => {
-            const response = await request(server).get('/api/qcFlags?sort[updatedAt]=DESC');
-            const { data: flags, meta: { page: { totalCount: count } } } = response.body;
-            expect(count).to.be.equal(5);
-            expect(flags).to.be.an('array');
-            expect(flags).to.be.lengthOf(5);
-            const fetchedSortedProperties = flags.map(({ updatedAt }) => updatedAt);
-            expect(fetchedSortedProperties).to.have.all.ordered.members([...fetchedSortedProperties].sort().reverse());
+            expect(data[0].qcFlagId).to.be.equal(5);
         });
 
         it('should support pagination', async () => {
-            const response = await request(server).get('/api/qcFlags?page[offset]=1&page[limit]=2&sort[id]=ASC');
+            const response = await request(server)
+                .get('/api/qcFlags/perDataPass?dataPassId=1&runNumber=106&dplDetectorId=1&page[offset]=1&page[limit]=2');
             expect(response.status).to.be.equal(200);
             const { data: qcFlags } = response.body;
             expect(qcFlags).to.be.an('array');
-            expect(qcFlags.map(({ id }) => id)).to.have.ordered.deep.members([2, 3]);
+            expect(qcFlags.map(({ qcFlagId }) => qcFlagId)).to.have.ordered.deep.members([2, 3]);
         });
         it('should return 400 when bad query paramter provided', async () => {
-            const response = await request(server).get('/api/qcFlags?a=1');
-            expect(response.status).to.be.equal(400);
-            const { errors } = response.body;
-            const titleError = errors.find((err) => err.source.pointer === '/data/attributes/query/a');
-            expect(titleError.detail).to.equal('"query.a" is not allowed');
+            {
+                const response = await request(server).get('/api/qcFlags/perDataPass?a=1');
+                expect(response.status).to.be.equal(400);
+                const { errors } = response.body;
+                const titleError = errors.find((err) => err.source.pointer === '/data/attributes/query/a');
+                expect(titleError.detail).to.equal('"query.a" is not allowed');
+            }
+            {
+                const response = await request(server).get('/api/qcFlags/perSimulationPass?a=1');
+                expect(response.status).to.be.equal(400);
+                const { errors } = response.body;
+                const titleError = errors.find((err) => err.source.pointer === '/data/attributes/query/a');
+                expect(titleError.detail).to.equal('"query.a" is not allowed');
+            }
         });
         it('should return 400 if the limit is below 1', async () => {
-            const response = await request(server).get('/api/qcFlags?page[limit]=0');
-            expect(response.status).to.be.equal(400);
-            const { errors } = response.body;
-            const titleError = errors.find((err) => err.source.pointer === '/data/attributes/query/page/limit');
-            expect(titleError.detail).to.equal('"query.page.limit" must be greater than or equal to 1');
+            {
+                const response = await request(server)
+                    .get('/api/qcFlags/perDataPass?dataPassId=1&runNumber=106&dplDetectorId=1&page[offset]=1&page[limit]=0');
+                expect(response.status).to.be.equal(400);
+                const { errors } = response.body;
+                const titleError = errors.find((err) => err.source.pointer === '/data/attributes/query/page/limit');
+                expect(titleError.detail).to.equal('"query.page.limit" must be greater than or equal to 1');
+            }
         });
     });
 
