a8f34720481ca11adf663ad3e6fd93d95aa48cd3
xsalonx <lukasz.dubiel3@gmail.com>
2024-04-24 12:02:53 +0200

    up

--- test/public/lhcPeriods/overview.test.js
+++ test/public/lhcPeriods/overview.test.js
@@ -150,11 +150,13 @@ module.exports = () => {
 
     it('should successfuly apply lhc period name filter', async () => {
         await goToPage(page, 'lhc-period-overview');
+        await page.waitForSelector('#reset-filters', { hidden: true, timeout: 250 });
         await pressElement(page, '#openFilterToggle');
         await fillInput(page, 'div.flex-row.items-baseline:nth-of-type(1) input[type=text]', 'LHC22a');
         await expectColumnValues(page, 'name', ['LHC22a']);
         await pressElement(page, '#reset-filters');
         await expectColumnValues(page, 'name', ['LHC23f', 'LHC22b', 'LHC22a']);
+        await page.waitForSelector('#reset-filters', { hidden: true, timeout: 250 });
     });
 
     it('should successfuly apply lhc period year filter', async () => {
