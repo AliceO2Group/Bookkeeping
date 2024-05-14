4de8433252593fe76a2e08a5ca7628656509168c
xsalonx <lukasz.dubiel3@gmail.com>
2024-05-14 12:37:08 +0200

    amend tests

--- test/lib/server/services/dpl/DplProcessService.test.js
+++ test/lib/server/services/dpl/DplProcessService.test.js
@@ -293,7 +293,7 @@ module.exports = () => {
                 },
             );
             expect(processExecution).to.not.be.null;
-            expect(processExecution.detectorId).to.equal(21);
+            expect(processExecution.detectorId).to.equal(22);
             const { name } = await getDplDetectorOrFail({ dplDetectorId: 21 });
             expect(name).to.equal('NON-EXISTING-DETECTOR');
             expect(processExecution.processId).to.equal(1);
