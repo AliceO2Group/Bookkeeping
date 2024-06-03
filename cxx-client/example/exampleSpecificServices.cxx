39c4e16f6a68697d5a6abb58bfdc6985767427ba
Martin Boulais <31805063+martinboulais@users.noreply.github.com>
2024-06-03 11:14:21 +0200

    [O2B-1268] Implement QC flags c++ API (#1577)

--- cxx-client/example/exampleSpecificServices.cxx
+++ cxx-client/example/exampleSpecificServices.cxx
@@ -23,14 +23,42 @@ int main(int argc, char** argv)
   }
 
   try {
-    // Test of FLP counters update
     auto client = BkpClientFactory::create(argv[1]);
+
+    // Test of FLP counters update
     client->flp()->updateReadoutCountersByFlpNameAndRunNumber("FLP-TPC-1", 1, 0xFFFFFFFFFFFFFFFF, 0xFFFFFFFFFFFFFFFF, 0xFFFFFFFFFFFFFFFF, 0xFFFFFFFFFFFFFFFF);
     std::cout << "FLP counters have been successfully updated" << std::endl;
 
-
     // Test of DPL process execution
     client->dplProcessExecution()->registerProcessExecution(106, o2::bkp::DplProcessType::QC_CHECKER, "SECOND-HOSTNAME", "PROCESS-NAME", "", "DEFAUlT");
+    std::cout << "DPL process execution has been successfully inserted" << std::endl;
+
+    // Test QC flag creation
+    auto dataPassQcFlagIds = client->qcFlag()->createForDataPass(
+      55,
+      "LHC22b_apass2",
+      "FT0",
+      { { 2, 1565280000000, 1565287200000, "FT0/Check" },
+        { .flagTypeId = 11, .origin = "FT0/task" } });
+
+    std::cout << "QC flags has been successfully inserted with ids ";
+    for (int flagIndex = 0; flagIndex < dataPassQcFlagIds.size() - 1; flagIndex++) {
+      std::cout << dataPassQcFlagIds[flagIndex] << ", ";
+    }
+    std::cout << dataPassQcFlagIds[dataPassQcFlagIds.size() - 1] << std::endl;
+
+    auto simulationPassQcFlagIds = client->qcFlag()->createForSimulationPass(
+      56,
+      "LHC23k6b",
+      "FT0",
+      { { 2, 1565294400000, 1565298000000, "FT0/Check" },
+        { .flagTypeId = 11, .origin = "FT0/task" } });
+
+    std::cout << "QC flags has been successfully inserted with ids ";
+    for (int flagIndex = 0; flagIndex < simulationPassQcFlagIds.size() - 1; flagIndex++) {
+      std::cout << simulationPassQcFlagIds[flagIndex] << ", ";
+    }
+    std::cout << simulationPassQcFlagIds[simulationPassQcFlagIds.size() - 1] << std::endl;
   } catch (std::runtime_error& error) {
     std::cerr << "An error occurred: " << error.what() << std::endl;
     exit(2);
