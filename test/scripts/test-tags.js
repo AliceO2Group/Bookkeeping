8c8ba41a63192eafb0ffa1249c1eac56c6e50409
Anwar Hamdi <anwar.hamdi@hva.nl>
2024-06-19 11:40:41 +0200

    [O2B-1300] Seperated local testing with docker containers

---
+++ test/scripts/test-tags.js
@@ -0,0 +1,34 @@
+/**
+ * @license
+ * Copyright CERN and copyright holders of ALICE O2. This software is
+ * distributed under the terms of the GNU General Public License v3 (GPL
+ * Version 3), copied verbatim in the file "COPYING".
+ *
+ * See http://alice-o2.web.cern.ch/license for full licensing information.
+ *
+ * In applying this license CERN does not waive the privileges and immunities
+ * granted to it by virtue of its status as an Intergovernmental Organization
+ * or submit itself to any jurisdiction.
+ */
+
+const application = require('../../lib/application');
+const OverviewSuite = require('../public/tags/overview.test');
+const DetailSuite = require('../public/tags/detail.test');
+const CreateSuite = require('../public/tags/create.test');
+
+describe('Bookkeeping', () => {
+    before(async () => {
+        await application.run();
+        await application.connectDatabase();
+    });
+
+    after(async () => {
+        await application.stop(true);
+    });
+
+    describe('Tags', () => {
+        describe('Overview Page', OverviewSuite);
+        describe('Detail Page', DetailSuite);
+        describe('Create Page', CreateSuite);
+    });
+});
