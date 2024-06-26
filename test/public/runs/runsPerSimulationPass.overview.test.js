a4407b641fa9800a04fdac677a0ce396ed9f3e40
xsalonx <lukasz.dubiel3@gmail.com>
2024-06-26 12:34:12 +0200

    fix tests

--- test/public/runs/runsPerSimulationPass.overview.test.js
+++ test/public/runs/runsPerSimulationPass.overview.test.js
@@ -108,7 +108,7 @@ module.exports = () => {
             inelasticInteractionRateAtEnd: (value) => value === '-' || !isNaN(Number(value.replace(/,/g, ''))),
             ...Object.fromEntries(DETECTORS.map((detectorName) => [
                 detectorName,
-                (qualityDisplay) => !qualityDisplay || /(QC)|(\d+!?)/.test(qualityDisplay),
+                (qualityDisplay) => !qualityDisplay || /(QC)|(\d+)/.test(qualityDisplay),
             ])),
         };
 
@@ -128,7 +128,7 @@ module.exports = () => {
         await reloadPage(page);
         await expectLink(page, 'tr#row56 .column-ITS a', {
             href: 'http://localhost:4000/?page=qc-flags-for-simulation-pass&runNumber=56&dplDetectorId=4&simulationPassId=2',
-            innerText: '0!',
+            innerText: '0',
         });
 
         await qcFlagService.delete(tmpQcFlag.id);
