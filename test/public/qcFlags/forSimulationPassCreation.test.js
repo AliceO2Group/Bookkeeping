f0ee7a330764dc9537b4d49b9f6099cd40aa3a7f
xsalonx <65893715+xsalonx@users.noreply.github.com>
2024-06-27 10:19:04 +0200

    Fix QC flags creation tests (#1633)
    * fix tests
    
    * add missing func

--- test/public/qcFlags/forSimulationPassCreation.test.js
+++ test/public/qcFlags/forSimulationPassCreation.test.js
@@ -19,11 +19,10 @@ const {
     pressElement,
     goToPage,
     waitForNavigation,
-    getColumnCellsInnerTexts,
     fillInput,
     expectColumnValues,
+    expectRowValues,
     expectUrlParams,
-    waitForTableLength,
 } = require('../defaults.js');
 const { resetDatabaseContent } = require('../../utilities/resetDatabaseContent.js');
 
@@ -122,9 +121,9 @@ module.exports = () => {
             dplDetectorId: '1',
         });
 
-        await waitForTableLength(page, 3);
-        const flagTypes = await getColumnCellsInnerTexts(page, 'flagType');
-        expect(flagTypes[0]).to.be.equal('Unknown Quality');
+        await expectRowValues(page, 1, {
+            flagType: 'Unknown Quality',
+        });
     });
 
     it('should successfully create time-based QC flag', async () => {
@@ -156,13 +155,11 @@ module.exports = () => {
             dplDetectorId: '1',
         });
 
-        await waitForTableLength(page, 4);
-        const flagTypes = await getColumnCellsInnerTexts(page, 'flagType');
-        const fromTimestamps = await getColumnCellsInnerTexts(page, 'from');
-        const toTimestamps = await getColumnCellsInnerTexts(page, 'to');
-        expect(flagTypes[0]).to.be.equal('Limited acceptance');
-        expect(fromTimestamps[0]).to.be.equal('08/08/2019\n13:01:01');
-        expect(toTimestamps[0]).to.be.equal('09/08/2019\n13:50:59');
+        await expectRowValues(page, 1, {
+            flagType: 'Limited acceptance',
+            from: '08/08/2019\n13:01:01',
+            to: '09/08/2019\n13:50:59',
+        });
     });
 
     it('should successfully create run-based QC flag in case of missing run start/stop', async () => {
