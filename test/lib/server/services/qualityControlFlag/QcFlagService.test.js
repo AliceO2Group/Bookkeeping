be1bc95a26a5a6e7513ed3a10738f959045d5ec3
xsalonx <lukasz.dubiel3@gmail.com>
2024-07-29 18:10:59 +0200

    testok

--- test/lib/server/services/qualityControlFlag/QcFlagService.test.js
+++ test/lib/server/services/qualityControlFlag/QcFlagService.test.js
@@ -1350,29 +1350,23 @@ module.exports = () => {
             const timeTrgEnd = t('22:00:00');
 
             const gaqSubsummaries = [
-                { from: t('06:00:00'), to: t('10:00:00'), bad: true, mcReproducible: false },
-                { from: t('10:00:00'), to: t('12:00:00'), bad: true, mcReproducible: false },
-                { from: t('12:00:00'), to: t('13:00:00'), bad: true, mcReproducible: true },
-                { from: t('13:00:00'), to: t('14:00:00'), bad: true, mcReproducible: true },
-                { from: t('14:00:00'), to: t('16:00:00'), bad: true, mcReproducible: false },
-                { from: t('18:00:00'), to: t('20:00:00'), bad: false, mcReproducible: false },
-                { from: t('20:00:00'), to: t('22:00:00'), bad: false, mcReproducible: false },
+                { from: t('06:00:00'), to: t('10:00:00'), numberOfFlags: 2, bad: true, mcReproducible: false },
+                { from: t('10:00:00'), to: t('12:00:00'), numberOfFlags: 3, bad: true, mcReproducible: false },
+                { from: t('12:00:00'), to: t('13:00:00'), numberOfFlags: 3, bad: true, mcReproducible: true },
+                { from: t('13:00:00'), to: t('14:00:00'), numberOfFlags: 2, bad: true, mcReproducible: true },
+                { from: t('14:00:00'), to: t('16:00:00'), numberOfFlags: 3, bad: true, mcReproducible: false },
+                { from: t('18:00:00'), to: t('20:00:00'), numberOfFlags: 2, bad: false, mcReproducible: false },
+                { from: t('20:00:00'), to: t('22:00:00'), numberOfFlags: 1, bad: false, mcReproducible: false },
             ];
-            // { from: t('06:00:00'), to: t('10:00:00'), quality: [bad, good] },
-            // { from: t('10:00:00'), to: t('12:00:00'), quality: [mc, bad, good] },
-            // { from: t('12:00:00'), to: t('13:00:00'), quality: [mc, mc, good] },
-            // { from: t('13:00:00'), to: t('14:00:00'), quality: [mc, good] },
-            // { from: t('14:00:00'), to: t('16:00:00'), quality: [good, good, bad] },
-            // { from: t('18:00:00'), to: t('20:00:00'), quality: [good, good] },
-            // { from: t('20:00:00'), to: t('22:00:00'), quality: [good] },
-            const expectedGaqSummary = gaqSubsummaries.reduce((acc, { from, to, bad, mcReproducible }) => {
+
+            const expectedGaqSummary = gaqSubsummaries.reduce((acc, { from, to, bad, mcReproducible, numberOfFlags }) => {
                 if (bad) {
                     acc.badEffectiveRunCoverage += to - from;
                 } else {
                     acc.explicitlyNotBadEffectiveRunCoverage += to - from;
                 }
                 acc.mcReproducible = acc.mcReproducible || mcReproducible;
-                acc.missingVerificationsCount ++;
+                acc.missingVerificationsCount += numberOfFlags;
                 return acc;
             }, { badEffectiveRunCoverage: 0, explicitlyNotBadEffectiveRunCoverage: 0, missingVerificationsCount: 0 });
             expectedGaqSummary.badEffectiveRunCoverage /= timeTrgEnd - timeTrgStart;
