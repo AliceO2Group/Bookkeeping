98356565aa4bc5f1127b3683c9d64018916deaeb
xsalonx <lukasz.dubiel3@gmail.com>
2024-08-01 14:47:59 +0200

    fix

--- test/public/qcFlags/gaqOverview.test.js
+++ test/public/qcFlags/gaqOverview.test.js
@@ -184,7 +184,7 @@ module.exports = () => {
         expect(await getPopoverInnerText(await page.waitForSelector('tbody tr:nth-of-type(2) td:nth-of-type(5) .popover-trigger')))
             .to.be.equal('This flag is not verified');
         await waitForNavigation(page, () => pressElement(page, 'tbody tr:nth-of-type(2) td:nth-of-type(5) a', true));
-        await pressElement(page, '#verify-qc-flag', true);
+        await pressElement(page, '#verify-qc-flag');
         await pressElement(page, '#submit', true);
         await waitForNavigation(page, () => pressElement(page, '#qc-flag-details-dataPass a', true));
         await waitForNavigation(page, () => pressElement(page, '#row106-globalAggregatedQuality a', true));
