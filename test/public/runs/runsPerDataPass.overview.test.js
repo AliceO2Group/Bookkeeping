2d83333d7fcf404e05e50803a2bdb3a416e76b8d
xsalonx <lukasz.dubiel3@gmail.com>
2024-08-20 15:25:28 +0200

    add test

--- test/public/runs/runsPerDataPass.overview.test.js
+++ test/public/runs/runsPerDataPass.overview.test.js
@@ -404,6 +404,23 @@ module.exports = () => {
         });
     }
 
+    it('should successfully apply gaqNotBadFraction filters', async () => {
+        await navigateToRunsPerDataPass(page, { lhcPeriodId: 2, dataPassId: 1 });
+
+        await pressElement(page, '#openFilterToggle');
+
+        const popoverSelector = await getPopoverSelector(await page.waitForSelector('.globalAggregatedQuality-filter .popover-trigger'));
+        await pressElement(page, `${popoverSelector} #gaqNotBadFraction-dropdown-option-ge`, true);
+        await fillInput(page, '#gaqNotBadFraction-value-input', '80');
+        await expectColumnValues(page, 'runNumber', ['106']);
+
+        await pressElement(page, '#mcReproducibleAsNotBadToggle input', true);
+        await expectColumnValues(page, 'runNumber', []);
+
+        await pressElement(page, '#reset-filters', true);
+        await expectColumnValues(page, 'runNumber', ['108', '107', '106']);
+    });
+
     it('should display bad runs marked out', async () => {
         await navigateToRunsPerDataPass(page, { lhcPeriodId: 2, dataPassId: 2 });
 
