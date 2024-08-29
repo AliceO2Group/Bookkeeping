1fbf275789518daf57c9dbc64a08b77c3934b751
xsalonx <lukasz.dubiel3@gmail.com>
2024-08-29 12:33:08 +0200

    add page test

---
+++ test/public/qcFlags/synchronousOverview.test.js
@@ -0,0 +1,87 @@
+/**
+ * @license
+ * Copyright CERN and copyright holders of ALICE O2. This software is
+ * distributed under the terms of the GNU General Public License v3 (GPL
+ * Version 3), copied verbatim in the file "COPYING".
+ *
+ * See http://alice-o2.web.cern.ch/license for full licensing information.
+ *
+ * In applying this license CERN does not waive the privileges and immunities
+ * granted to it by virtue of its status as an Intergovernmental Organization
+ * or submit itself to any jurisdiction.
+ */
+
+const chai = require('chai');
+const {
+    defaultBefore,
+    defaultAfter,
+    expectInnerText,
+    pressElement,
+    goToPage,
+    validateTableData,
+    expectUrlParams,
+    waitForNavigation,
+    getColumnCellsInnerTexts,
+} = require('../defaults.js');
+
+const { expect } = chai;
+const dateAndTime = require('date-and-time');
+const { resetDatabaseContent } = require('../../utilities/resetDatabaseContent.js');
+
+module.exports = () => {
+    let page;
+    let browser;
+
+    before(async () => {
+        [page, browser] = await defaultBefore(page, browser);
+        await page.setViewport({
+            width: 1200,
+            height: 720,
+            deviceScaleFactor: 1,
+        });
+        await resetDatabaseContent();
+    });
+
+    after(async () => {
+        [page, browser] = await defaultAfter(page, browser);
+    });
+
+    it('loads the page successfully', async () => {
+        const response = await goToPage(page, 'synchronous-qc-flags', { queryParameters: { runNumber: 56, dplDetectorId: 7 } });
+
+        expect(response.status()).to.equal(200);
+        expect(await page.title()).to.equal('AliceO2 Bookkeeping');
+
+        await expectInnerText(page, 'h2:nth-of-type(1)', 'Sync QC');
+        await expectInnerText(page, 'h2:nth-of-type(2)', '56');
+        await expectInnerText(page, 'h2:nth-of-type(3)', 'FT0');
+    });
+
+    it('shows correct datatypes in respective columns', async () => {
+        // eslint-disable-next-line require-jsdoc
+        const validateDate = (date) => date === '-' || !isNaN(dateAndTime.parse(date, 'DD/MM/YYYY hh:mm:ss'));
+        const tableDataValidators = {
+            flagType: (flagType) => flagType && flagType !== '-',
+            createdBy: (userName) => userName && userName !== '-',
+            from: (timestamp) => timestamp === 'Whole run coverage' || validateDate(timestamp),
+            to: (timestamp) => timestamp === 'Whole run coverage' || validateDate(timestamp),
+            createdAt: validateDate,
+            updatedAt: validateDate,
+        };
+
+        await validateTableData(page, new Map(Object.entries(tableDataValidators)));
+
+        expect(await getColumnCellsInnerTexts(page, 'flagType'), ['Bad PID', 'Good']);
+    });
+
+    it('Should display the correct items counter at the bottom of the page', async () => {
+        await expectInnerText(page, '#firstRowIndex', '1');
+        await expectInnerText(page, '#lastRowIndex', '2');
+        await expectInnerText(page, '#totalRowsCount', '2');
+    });
+
+    it('can navigate to run details page from breadcrumbs link', async () => {
+        await waitForNavigation(page, () => pressElement(page, 'h2:nth-of-type(3)'));
+        expectUrlParams(page, { page: 'run-detail', runNumber: '106' });
+    });
+};
