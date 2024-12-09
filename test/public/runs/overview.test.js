f1fe315fd6125a2d3f33a6fd1c06a1080e33267a
Jaden505 <jadenxxxl@gmail.com>
2024-12-09 11:57:43 +0100

    remove unnecessary changes

--- test/public/runs/overview.test.js
+++ test/public/runs/overview.test.js
@@ -277,7 +277,7 @@ module.exports = () => {
     it('Should have balloon on detector, tags and eor column', async () => {
         await goToPage(page, 'run-overview');
 
-        await pressElement(page, '#openFilterToggle', true);
+        await pressElement(page, '#openFilterToggle');
         await page.waitForSelector(runNumberInputSelector);
 
         // Run 106 has detectors and tags that overflow
@@ -287,7 +287,7 @@ module.exports = () => {
         await checkColumnBalloon(page, 1, 2);
         await checkColumnBalloon(page, 1, 3);
 
-        await pressElement(page, '#openFilterToggle', true);
+        await pressElement(page, '#openFilterToggle');
         await page.waitForSelector(runNumberInputSelector);
 
         // Run 1 has eor reasons that overflow
@@ -306,7 +306,7 @@ module.exports = () => {
     it('should successfully filter on detectors', async () => {
         // Open filter toggle
         await pressElement(page, '#openFilterToggle');
-        await page.waitForSelector('.detectors-filter .dropdown-trigger', { visible: true });
+        await page.waitForSelector('.detectors-filter .dropdown-trigger');
 
         await pressElement(page, '.detectors-filter .dropdown-trigger');
         await pressElement(page, '#detector-filter-dropdown-option-ITS', true);
@@ -441,7 +441,7 @@ module.exports = () => {
         await goToPage(page, 'run-overview');
 
         // Open the filters
-        await pressElement(page, '#openFilterToggle', true);
+        await pressElement(page, '#openFilterToggle');
         await page.waitForSelector('#o2startFilterFromTime');
         let today = new Date();
         today.setMinutes(today.getMinutes() - today.getTimezoneOffset());
@@ -470,7 +470,7 @@ module.exports = () => {
         const validValue = '2021-03-21';
         // Open the filters
         await pressElement(page, '#openFilterToggle');
-        await page.waitForSelector('#eorDescription', { visible: true });
+        await page.waitForSelector('#eorDescription');
 
         // Set date
         for (const key in dateFilterSelectors) {
@@ -491,7 +491,7 @@ module.exports = () => {
         const dateString = '03-02-2021';
         // Open the filters
         await pressElement(page, '#openFilterToggle');
-        await page.waitForSelector('#eorDescription', { visible: true });
+        await page.waitForSelector('#eorDescription');
 
         // Set date to an open day
         for (const selector of Object.values(dateFilterSelectors)) {
@@ -524,8 +524,8 @@ module.exports = () => {
         const maxTime = '23:59';
         const minTime = '00:00';
         // Open the filters
-        await pressElement(page, '#openFilterToggle', true);
-        await page.waitForSelector('#eorDescription', { visible: true });
+        await pressElement(page, '#openFilterToggle');
+        await page.waitForSelector('#eorDescription');
         // Set date to an open day
         for (const selector of Object.values(dateFilterSelectors)) {
             await page.type(selector, dateString);
@@ -546,7 +546,7 @@ module.exports = () => {
     it('should successfully filter on duration', async () => {
         await goToPage(page, 'run-overview');
 
-        await pressElement(page, '#openFilterToggle', true);
+        await pressElement(page, '#openFilterToggle');
         await page.waitForSelector('#duration-operator');
 
         const runDurationOperatorSelector = '#duration-operator';
@@ -630,7 +630,7 @@ module.exports = () => {
         };
 
         // Open filter toggle
-        await pressElement(page, '#openFilterToggle', true);
+        await pressElement(page, '#openFilterToggle');
         await page.waitForSelector(badFilterSelector);
 
         await page.$eval(badFilterSelector, (element) => element.click());
@@ -683,7 +683,7 @@ module.exports = () => {
         };
 
         // Open filter toggle
-        await pressElement(page, '#openFilterToggle', true);
+        await pressElement(page, '#openFilterToggle');
         await page.waitForSelector(offFilterSelector);
 
         await page.$eval(offFilterSelector, (element) => element.click());
@@ -982,7 +982,7 @@ module.exports = () => {
     it('should successfully display runs export button', async () => {
         await goToPage(page, 'run-overview');
         await page.waitForSelector(EXPORT_RUNS_TRIGGER_SELECTOR);
-        const runsExportButton = await page.$(EXPORT_RUNS_TRIGGER_SELECTOR, { visible: true });
+        const runsExportButton = await page.$(EXPORT_RUNS_TRIGGER_SELECTOR);
         expect(runsExportButton).to.be.not.null;
     });
 
