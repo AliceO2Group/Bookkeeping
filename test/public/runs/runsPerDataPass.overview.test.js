ac0f77b5e70fffcf5d4f27589d3aa6552287985a
xsalonx <lukasz.dubiel3@gmail.com>
2025-07-29 12:13:21 +0200

    fix

--- test/public/runs/runsPerDataPass.overview.test.js
+++ test/public/runs/runsPerDataPass.overview.test.js
@@ -418,15 +418,13 @@ module.exports = () => {
     });
 
     it('should successfully apply detectors notBadFraction filters', async () => {
-        await pressElement(page, '#openFilterToggle', true);
-
         await page.waitForSelector('#detectorsQc-for-1-notBadFraction-operator');
         await page.select('#detectorsQc-for-1-notBadFraction-operator', '<=');
-        await fillInput(page, '#detectorsQc-for-1-notBadFraction-operand', '70', ['change']);
+        await fillInput(page, '#detectorsQc-for-1-notBadFraction-operand', '90', ['change']);
         await expectColumnValues(page, 'runNumber', ['106']);
 
         await pressElement(page, '#mcReproducibleAsNotBadToggle input', true);
-        await expectColumnValues(page, 'runNumber', []);
+        await expectColumnValues(page, 'runNumber', ['107', '106']);
 
         await pressElement(page, '#openFilterToggle', true);
         await pressElement(page, '#reset-filters', true);
