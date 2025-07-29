eeb1f2f3ade8effe83b04b8654b02627270a0543
xsalonx <lukasz.dubiel3@gmail.com>
2025-07-29 10:10:41 +0200

    fix

--- test/lib/usecases/run/GetAllRunsUseCase.test.js
+++ test/lib/usecases/run/GetAllRunsUseCase.test.js
@@ -795,4 +795,58 @@ module.exports = () => {
             expect(runs).to.have.lengthOf(0);
         }
     });
+
+    it('should successfully filter by detectors\' notBadFraction', async () => {
+        const dataPassIds = [1];
+        {
+            const { runs } = await new GetAllRunsUseCase().execute({
+                query: {
+                    filter: {
+                        dataPassIds,
+                        detectorsQc: {  '_1': { notBadFraction: { operator: '<', limit: 0.7 } } },
+                    },
+                },
+            });
+            expect(runs).to.be.an('array');
+            expect(runs.map(({ runNumber }) => runNumber)).to.have.all.members([106]);
+        }
+        {
+            const { runs } = await new GetAllRunsUseCase().execute({
+                query: {
+                    filter: {
+                        dataPassIds,
+                        detectorsQc: {  '_1': { notBadFraction: { operator: '<', limit: 0.8 } } },
+                    },
+                },
+            });
+            expect(runs.map(({ runNumber }) => runNumber)).to.have.all.members([107, 106]);
+        }
+
+        {
+            const { runs } = await new GetAllRunsUseCase().execute({
+                query: {
+                    filter: {
+                        dataPassIds,
+                        detectorsQc: {  '_1': { notBadFraction: { operator: '<', limit: 0.8 } }, mcReproducibleAsNotBad: true },
+                    },
+                },
+            });
+            expect(runs.map(({ runNumber }) => runNumber)).to.have.all.members([106]);
+        }
+
+        {
+            const { runs } = await new GetAllRunsUseCase().execute({
+                query: {
+                    filter: {
+                        dataPassIds,
+                        detectorsQc: {
+                            '_2': { notBadFraction: { operator: '>', limit: 0.8 } },
+                            '_1': { notBadFraction: {operator: '<', limit: 0.8 } },
+                        },
+                    },
+                },
+            });
+            expect(runs.map(({ runNumber }) => runNumber)).to.have.all.members([107]);
+        }
+    });
 };
