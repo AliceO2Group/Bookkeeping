64a143886f8c521d8ecbc9836d4634a7d012a7c8
xsalonx <65893715+xsalonx@users.noreply.github.com>
2025-07-30 16:05:33 +0200

    [O2B-1240] Display only physical productions by default and adds filtering for 'test' and 'debug' productions display (#1940)
    * add logic on backend
    
    * wip
    
    * wip
    
    * add filter
    
    * user enum
    
    * refactor WIP
    
    * w
    
    * fix sorting
    
    * cleanup
    
    * api test
    
    * api test
    
    * ui test
    
    * fix
    
    * fix counting
    
    * fix test
    
    * refactor test
    
    * use getter
    
    * raw
    
    * refactor
    
    * refactor
    
    * cleanup
    
    * cleanup
    
    * cleanup
    
    * docs
    
    * fix tesT
    
    * fixeD

--- test/public/dataPasses/overviewPerSimulationPass.test.js
+++ test/public/dataPasses/overviewPerSimulationPass.test.js
@@ -113,7 +113,7 @@ module.exports = () => {
     it('should successfully apply data pass name filter', async () => {
         await pressElement(page, '#openFilterToggle');
 
-        await fillInput(page, 'div.flex-row.items-baseline:nth-of-type(1) input[type=text]', 'LHC22b_apass1');
+        await fillInput(page, 'div.flex-row.items-baseline:nth-of-type(1) input[type=text]', 'LHC22b_apass1', ['change']);
         await expectColumnValues(page, 'name', ['deleted\nLHC22b_apass1\nSkimmable']);
 
         await pressElement(page, '#reset-filters', true);
