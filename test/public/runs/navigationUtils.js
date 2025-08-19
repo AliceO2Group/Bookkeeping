0ce697b97fb533595ef0b36ca787b5682ee9e0e1
xsalonx <65893715+xsalonx@users.noreply.github.com>
2025-08-19 15:48:30 +0200

    [O2B-1467] Add MUON and AOT columns (#1951)
    * add
    
    * add other typeS
    
    * cleanup
    
    * allow multiple sources in obervable data and ref FilteringModel
    
    * rm error
    
    * add new columns to runs per lhc period
    
    * cleanup
    
    * rename migration
    
    * type
    
    * special handling of TST and ZDC
    
    * roll bakc
    
    * roll bakc
    
    * add test
    
    * fix tests
    
    * fix
    
    * fix
    
    * fix
    
    * fix
    
    * fix
    
    * fix
    
    * fixes
    
    * fix
    
    * fix
    
    * fixes
    
    * fix
    
    * fix
    
    * fix
    
    * fixes, grant more privileges in dev mode
    
    * rename var
    
    * rename
    
    * update getQCdetector
    
    * explicie types

--- test/public/runs/navigationUtils.js
+++ test/public/runs/navigationUtils.js
@@ -24,12 +24,11 @@ const { waitForNavigation, pressElement, getInnerText, expectUrlParams, waitForT
  */
 exports.navigateToRunsPerDataPass = async (page, lhcPeriodId, dataPassId, expectedRowsCount) => {
     await waitForNavigation(page, () => pressElement(page, 'a#lhc-period-overview', true));
-    const pdpBeamType = await getInnerText(await page.waitForSelector(`#row${lhcPeriodId}-beamTypes`));
     await waitForNavigation(page, () => pressElement(page, `#row${lhcPeriodId}-associatedDataPasses a`, true));
     expectUrlParams(page, { page: 'data-passes-per-lhc-period-overview', lhcPeriodId });
     await page.waitForSelector('th#description');
     await waitForNavigation(page, () => pressElement(page, `#row${dataPassId}-associatedRuns a`, true));
-    expectUrlParams(page, { page: 'runs-per-data-pass', dataPassId, pdpBeamType });
+    expectUrlParams(page, { page: 'runs-per-data-pass', dataPassId }, ['pdpBeamType']);
     await waitForTableLength(page, expectedRowsCount);
 };
 
