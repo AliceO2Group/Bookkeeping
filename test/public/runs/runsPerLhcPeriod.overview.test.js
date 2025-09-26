303d35629a8c1ea4ec5372bed015482df994b97c
xsalonx <65893715+xsalonx@users.noreply.github.com>
2025-09-26 15:56:07 +0200

    [O2B-1397] Do not change pagination after going back to RunsPer<STH> page with same scope identifier (#1992)
    * add logic to runs per data pass model
    
    * aply to other views
    
    * apply to LhcPeriodsOverview
    
    * fix

--- test/public/runs/runsPerLhcPeriod.overview.test.js
+++ test/public/runs/runsPerLhcPeriod.overview.test.js
@@ -31,6 +31,7 @@ const {
     fillInput,
     expectColumnValues,
     openFilteringPanel,
+    resetFilters,
 } = require('../defaults.js');
 const { RUN_QUALITIES, RunQualities } = require('../../../lib/domain/enums/RunQualities.js');
 const { resetDatabaseContent } = require('../../utilities/resetDatabaseContent.js');
@@ -234,16 +235,21 @@ module.exports = () => {
         ]);
 
         fs.unlinkSync(path.resolve(downloadPath, targetFileName));
+        await page.evaluate(() => {
+            // eslint-disable-next-line no-undef
+            model.runs.perLhcPeriodOverviewModel.reset();
+        });
     });
 
     it('can navigate to a run detail page', async () => {
         const expectedRunNumber = await getInnerText(await page.waitForSelector('tbody tr:first-of-type a'));
         await waitForNavigation(page, () => pressElement(page, 'tbody tr:first-of-type a'));
         expectUrlParams(page, { page: 'run-detail', runNumber: expectedRunNumber });
-        await page.goBack()
+        await page.goBack();
     });
 
     it('should successfully apply detectors notBadFraction filters', async () => {
+        await navigateToRunsPerLhcPeriod(page, 1, 4);
         await openFilteringPanel(page);
 
         await page.waitForSelector('#inelasticInteractionRateAvg-operator');
@@ -251,8 +257,7 @@ module.exports = () => {
         await fillInput(page, '#inelasticInteractionRateAvg-operand', '100000', ['change']);
         await expectColumnValues(page, 'runNumber', ['56', '54']);
 
-        await pressElement(page, '#openFilterToggle', true);
-        await pressElement(page, '#reset-filters', true);
+        await resetFilters(page);
         await expectColumnValues(page, 'runNumber', ['105', '56', '54', '49']);
     });
 
