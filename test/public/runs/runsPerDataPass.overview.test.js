a5768c79d779810dd08f5ab802b8d83fecc16b12
Isaac Hill <71404865+isaachilly@users.noreply.github.com>
2026-01-30 18:12:22 +0100

    [O2B-1528] Update GAQ test with enabling of GAQ summary again
    Adjust the runsPerDataPass overview test to match updated row and expected GAQ values.

--- test/public/runs/runsPerDataPass.overview.test.js
+++ test/public/runs/runsPerDataPass.overview.test.js
@@ -147,8 +147,9 @@ module.exports = () => {
 
         await expectInnerText(page, '#row106-globalAggregatedQuality', 'GAQ');
 
-        expect(await getPopoverInnerText(await page.waitForSelector('#row106-globalAggregatedQuality .popover-trigger')))
-            .to.be.equal('GAQ Summary is disabled, please click to view GAQ flags');
+        await expectInnerText(page, '#row107-globalAggregatedQuality', '76');
+        expect(await getPopoverInnerText(await page.waitForSelector('#row107-globalAggregatedQuality .popover-trigger')))
+            .to.be.equal('Missing 3 verifications');
     });
 
     it('should ignore QC flags created by services in QC summaries of AOT and MUON ', async () => {
