2c79c9fa34e5852e3cff5a23fe779a284892ee18
NarrowsProjects <guust.metz@cern.ch>
2026-04-29 10:38:47 +0200

    add warning tests

---
+++ test/public/components/warnings.test.js
@@ -0,0 +1,40 @@
+/**
+ * @license
+ * Copyright CERN and copyright holders of ALICE O2. This software is
+ * distributed under the terms of the GNU General Public License v3 (GPL
+ * Version 3), copied verbatim in the file "COPYING".
+ *
+ * See http://alice-o2.web.cern.ch/license for full licensing information.
+ *
+ * In applying this license CERN does not waive the privileges and immunities
+ * granted to it by virtue of its status as an Intergovernmental Organization
+ * or submit itself to any jurisdiction.
+ */
+
+const { expect } = require('chai');
+const {
+    defaultBefore,
+    defaultAfter,
+    getInnerText,
+} = require('../defaults.js');
+
+module.exports = () => {
+    let page;
+    let browser;
+    const navTabSelector = 'a.btn-tab[id]'; // ALI FLP doesn't have an id, nor a functional href, hence why it is excluded like this
+
+    before(async () => {
+        [page, browser] = await defaultBefore();
+    });
+
+    it('Should show warning when a filter in the url is not recognised', async () => {
+        await page.goto('http://localhost:4000/?page=log-overview&filter[fake]=fake', { waitUntil: 'load' });
+        const warningText = await getInnerText('.alert-warning > ul');
+
+        expect(warningText).to.equal('Unknown Filters: fake');
+    });
+
+    after(async () => {
+        await defaultAfter(page, browser);
+    });
+};
