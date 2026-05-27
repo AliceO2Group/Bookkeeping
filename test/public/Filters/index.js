ee0dda9008174e8862e2612a432a790bc0862d54
NarrowsProjects <guust.metz@cern.ch>
2026-05-27 14:38:06 +0200

    test: uncomment a test suite

--- test/public/Filters/index.js
+++ test/public/Filters/index.js
@@ -15,6 +15,6 @@ const ToUrlSuite = require('./filtersToUrl.test.js');
 const ToFilterSuite = require('./urlToFilter.test.js');
 
 module.exports = () => {
-    // describe('Filters to URL', ToUrlSuite);
+    describe('Filters to URL', ToUrlSuite);
     describe('URL to Filters', ToFilterSuite);
 };
