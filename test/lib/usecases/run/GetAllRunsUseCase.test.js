29f47990c493cb413000595be039c065094380c1
NarrowsProjects <guust.metz@cern.ch>
2026-06-24 11:51:46 +0200

    chore: add missing semicolon

--- test/lib/usecases/run/GetAllRunsUseCase.test.js
+++ test/lib/usecases/run/GetAllRunsUseCase.test.js
@@ -43,7 +43,7 @@ module.exports = () => {
             }
             expect(comparitor(fileCount)).to.be.true;
         }
-    }
+    };
 
     beforeEach(async () => {
         getAllRunsDto = await GetAllRunsDto.validateAsync({});
