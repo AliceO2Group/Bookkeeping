19ab138d5921c7e68fad17b62e214c0eb1ca1a75
George R. <george.raduta@cern.ch>
2026-08-04 16:14:52 +0200

    [O2B-1602] Add endpoint retrieval of pdp beam types and runs endpoint filtering option (#2203)
    - add new runs/pdpBeamType endpoint to fetch all uniquely identified from DB pdpBeamTypes when deploying an environment/starting a run
    - add option for GET runs endpoint to filter by pdpBeamType
    - expand the BeamTypesDto to provide a validation checker for the pdpBeamType

---
+++ test/lib/server/services/pdpBeamTypes/getAllPdpBeamTypes.test.js
@@ -0,0 +1,28 @@
+/**
+ * @license
+ * Copyright CERN and copyright holders of ALICE O2. This software is
+ * distributed under the terms of the GNU General Public License v3 (GPL
+ * Version 3), copied verbatim in the file "COPYING".
+ *
+ * See http://alice-o2.web.cern.ch/license for full licensing information.
+ *
+ * In applying this license CERN does not waive the privileges and immunities
+ * granted to it by virtue of its status as an Intergovernmental Organization
+ * or submit itself to any jurisdiction.
+ */
+
+const { expect } = require('chai');
+const { getAllPdpBeamTypes } =  require('../../../../../lib/server/services/beam/getAllPdpBeamTypes.js');
+
+module.exports = () => {
+    it('should successfully return the full list of not null PDP beam types from runs table', async () => {
+        const pdpBeamTypes = await getAllPdpBeamTypes();
+        expect(pdpBeamTypes.map(({ dataValues: { name } }) => ({ name }))).to.deep.eq([
+            { name: 'pp' },
+            { name: 'PbPb' },
+            { name: 'technical' },
+            { name: 'cosmic' },
+            { name: 'OO' },
+        ]);
+    });
+};
